e86fc47abffa87100b4e4542a915f03aa65cb296
shraddhabajare <shraddhab.ska@gmail.com>
2022-04-08 16:54:48 +0530

    AT1-1089 - Add helper classes.

--- tests/integration/test_telescopeon_command.py
+++ tests/integration/test_telescopeon_command.py
@@ -3,21 +3,19 @@ import time
 
 import pytest
 from tango import DeviceProxy, DevState
-
-LOGGER = logging.getLogger(__name__)
-
 import pytest
-import time
-
+import os
 import logging
-import pytest
-from tango import DeviceProxy, DevState
+from resources.test_support.helpers import waiter, watch, resource
+from resources.test_support.controls import telescope_is_in_standby, telescope_is_in_on, telescope_is_in_off
+import resources.test_support.tmc_helpers as tmc
+
 
 LOGGER = logging.getLogger(__name__)
 
-TMCCentralNode = DeviceProxy("ska_mid/tm_central/central_node")
-SdpMaster = DeviceProxy("mid_sdp/elt/master")
-SdpSubarray = DeviceProxy("mid_sdp/elt/subarray_1")
+central_node = DeviceProxy("ska_mid/tm_central/central_node")
+sdp_master = DeviceProxy("mid_sdp/elt/master")
+sdp_Subarray = DeviceProxy("mid_sdp/elt/subarray_1")
 
 @pytest.mark.SKA_mid
 def test_telescope_on():
@@ -26,52 +24,61 @@ def test_telescope_on():
         fixture = {}
         fixture["state"] = "Unknown"
 
-        """Verify Telescope is Off"""
-        assert SdpMaster.State() in [DevState.DISABLE, DevState.STANDBY, DevState.OFF]
-        assert SdpSubarray.State() in [DevState.DISABLE, DevState.OFF]
-
+        # """Verify Telescope is Off"""
+        # assert sdp_master.State() in [DevState.DISABLE, DevState.STANDBY, DevState.OFF]
+        # assert SdpSubarray.State() in [DevState.DISABLE, DevState.OFF]
+        assert telescope_is_in_standby()
+        LOGGER.info("Staring up the Telescope")
         """Invoke TelescopeOn() command on TMC"""
         LOGGER.info("Invoking TelescopeOn command on TMC CentralNode")
-        TMCCentralNode.TelescopeOn()
-        time.sleep(0.5)
+        # central_node.TelescopeOn()
+        # time.sleep(0.5)
+        tmc.set_to_on()
+
         LOGGER.info("TelescopeOn command is invoked successfully")
 
-        """Verify Sdp Master and Sdp Subarray State"""
-        assert TMCCentralNode.State() == DevState.ON
-        assert TMCCentralNode.telescopeState == DevState.UNKNOWN
-        assert SdpMaster.State() == DevState.ON
-        assert SdpSubarray.State() == DevState.ON
+        # """Verify Sdp Master and Sdp Subarray State"""
+        # assert central_node.State() == DevState.ON
+        # assert central_node.telescopeState == DevState.UNKNOWN
+        # assert sdp_master.State() == DevState.ON
+        # assert sdp_Subarray.State() == DevState.ON
+
+        assert telescope_is_in_on()
 
         fixture["state"] = "TelescopeOn"
 
-        """Invoke TelescopeOff() command on TMC"""
-        LOGGER.info("Invoking TelescopeOff command on TMC CentralNode")
-        TMCCentralNode.TelescopeOff()
-        time.sleep(0.5)
-        LOGGER.info("TelescopeOff command is invoked successfully")
+        # """Invoke TelescopeOff() command on TMC"""
+        # LOGGER.info("Invoking TelescopeOff command on TMC CentralNode")
+        # central_node.TelescopeOff()
+        # time.sleep(0.5)
+        # LOGGER.info("TelescopeOff command is invoked successfully")
 
-        """Verify Sdp Master and Sdp Subarray State"""
-        assert TMCCentralNode.State() == DevState.ON
-        assert TMCCentralNode.telescopeState == DevState.UNKNOWN
-        assert SdpMaster.State() == DevState.OFF
-        assert SdpSubarray.State() == DevState.OFF
+        tmc.set_to_off()
+
+        # """Verify Sdp Master and Sdp Subarray State"""
+        # assert central_node.State() == DevState.ON
+        # assert central_node.telescopeState == DevState.UNKNOWN
+        # assert sdp_master.State() == DevState.OFF
+        # assert sdp_Subarray.State() == DevState.OFF
+
+        assert telescope_is_in_off()
 
         fixture["state"] = "TelescopeOff"
 
         LOGGER.info("Tests complete: tearing down...")
 
-    except Exception:
-        LOGGER.info("Tear down the test...")
-        tear_down(fixture)
-        pytest.fail("unable to complete test without exceptions")
+    except:
+        LOGGER.info("Tearing down failed test, state = {}".format(fixture["state"]))
+        if fixture["state"] == "TelescopeOn":
+            tmc.set_to_off()
+        raise
 
-
-def tear_down(fixture):
-    LOGGER.info(
-        "Tearing down failed test, state = {}".format(fixture["state"])
-    )
-    if fixture["state"] == "TelescopeOn":
-        TMCCentralNode.TelescopeOff()
-    else:
-        pytest.fail("unable to complete test without exceptions")
+# def tear_down(fixture):
+#     LOGGER.info(
+#         "Tearing down failed test, state = {}".format(fixture["state"])
+#     )
+#     if fixture["state"] == "TelescopeOn":
+#         central_node.TelescopeOff()
+#     else:
+#         pytest.fail("unable to complete test without exceptions")
 
