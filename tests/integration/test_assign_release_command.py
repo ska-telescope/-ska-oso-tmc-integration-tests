860d208cd9d473f9adf3bdc0ace3479c56f08db6
shraddhabajare <shraddhab.ska@gmail.com>
2022-05-19 11:29:35 +0530

    AT1-1124 - Increment helm chart version

--- tests/integration/test_assign_release_command.py
+++ tests/integration/test_assign_release_command.py
@@ -11,7 +11,7 @@ assign_resources_file = "command_AssignResources.json"
 release_resources_file  = "command_ReleaseResources.json"
 
 @pytest.mark.SKA_mid
-def test_assign_release_commands():
+def test_assign_release():
     """AssignResources and ReleaseResources is executed."""
     try:
         fixture = {}
