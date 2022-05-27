255fefc0fc7325e52a361a7a79bc18afd52e89c0
Kalyani Thigale <kalyanit.ska@gmail.com>
2022-05-27 11:14:39 +0530

    Modify TelescopeOn-Off test case

--- tests/resources/test_support/tmc_helpers.py
+++ tests/resources/test_support/tmc_helpers.py
@@ -6,7 +6,7 @@ from tests.resources.test_support.sync_decorators import (
     sync_release_resources,
 )
 from tango import DeviceProxy, DevState
-from tests.resources.test_support.controls import centralnode, csp_subarray1
+from tests.resources.test_support.controls import centralnode, csp_subarray1, sdp_subarray1
 
 import logging
 
@@ -27,12 +27,14 @@ def set_to_on():
     CentralNode.TelescopeOn()
     csp_Subarray1_proxy = DeviceProxy(csp_subarray1)
     csp_Subarray1_proxy.SetDirectState(DevState.ON)
+    sdp_Subarray1_proxy = DeviceProxy(sdp_subarray1)
+    sdp_Subarray1_proxy.SetDirectState(DevState.ON)
 
 @sync_set_to_off
 def set_to_off():
     CentralNode = DeviceProxy(centralnode)
     CentralNode.TelescopeOff()
-    csp_Subarray1_proxy = DeviceProxy(csp_subarray1)
+    csp_Subarray1_proxy = DeviceProxy(sdp_subarray1)
     csp_Subarray1_proxy.SetDirectState(DevState.OFF)
     LOGGER.info(
             f"After invoking TelescopeOff command {centralnode} State is: {CentralNode.State()}"
