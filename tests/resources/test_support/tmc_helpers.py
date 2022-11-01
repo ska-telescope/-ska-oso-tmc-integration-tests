3ed75f5aaacc4283859a4ea4556c500d72cc951f
muskansharma <muskans.ska@gmail.com>
2022-11-01 12:13:12 +0530

    HM-81:Resolved review comments

--- tests/resources/test_support/tmc_helpers.py
+++ tests/resources/test_support/tmc_helpers.py
@@ -6,7 +6,7 @@ from tests.resources.test_support.sync_decorators import (
     sync_release_resources,
 )
 from tango import DeviceProxy, DevState
-from tests.resources.test_support.controls import centralnode, csp_subarray1, sdp_subarray1, dish_master1, tm_subarraynode1
+from tests.resources.test_support.controls import central_node, csp_subarray1, sdp_subarray1, dish_master1, tm_subarraynode1
 
 import logging
 
@@ -19,7 +19,7 @@ def get_input_str(input_file):
         return f.read()
 
 def check_devices():
-    CentralNode = DeviceProxy(centralnode)
+    CentralNode = DeviceProxy(central_node)
     assert 0 < CentralNode.ping()
     csp_Subarray1_proxy = DeviceProxy(csp_subarray1)
     assert 0 < csp_Subarray1_proxy.ping()
@@ -50,9 +50,9 @@ def check_devices():
 
 @sync_telescope_on
 def set_to_on():
-    CentralNode = DeviceProxy(centralnode)
+    CentralNode = DeviceProxy(central_node)
     LOGGER.info(
-        f"Before Sending TelescopeOn command {centralnode} State is: {CentralNode.State()}"
+        f"Before Sending TelescopeOn command {central_node} State is: {CentralNode.State()}"
     )
     CentralNode.TelescopeOn()
     csp_Subarray1_proxy = DeviceProxy(csp_subarray1)
@@ -64,7 +64,7 @@ def set_to_on():
 
 @sync_set_to_off
 def set_to_off():
-    CentralNode = DeviceProxy(centralnode)
+    CentralNode = DeviceProxy(central_node)
     CentralNode.TelescopeOff()
     csp_Subarray1_proxy = DeviceProxy(csp_subarray1)
     csp_Subarray1_proxy.SetDirectState(DevState.OFF)
@@ -73,12 +73,12 @@ def set_to_off():
     dish_master1_proxy = DeviceProxy(dish_master1)
     dish_master1_proxy.SetDirectState(DevState.STANDBY)
     LOGGER.info(
-            f"After invoking TelescopeOff command {centralnode} State is: {CentralNode.State()}"
+            f"After invoking TelescopeOff command {central_node} State is: {CentralNode.State()}"
     )
     
 @sync_set_to_standby
 def set_to_standby():
-    CentralNode = DeviceProxy(centralnode)
+    CentralNode = DeviceProxy(central_node)
     CentralNode.TelescopeStandBy()
     csp_Subarray1_proxy = DeviceProxy(csp_subarray1)
     csp_Subarray1_proxy.SetDirectState(DevState.OFF)
@@ -87,13 +87,13 @@ def set_to_standby():
     dish_master1_proxy = DeviceProxy(dish_master1)
     dish_master1_proxy.SetDirectState(DevState.STANDBY)
     LOGGER.info(
-            f"After invoking TelescopeStandBy command {centralnode} State is: {CentralNode.State()}"
+            f"After invoking TelescopeStandBy command {central_node} State is: {CentralNode.State()}"
     )
 
 @sync_release_resources
 def invoke_releaseResources(release_input_str):
-    CentralNode = DeviceProxy(centralnode)
+    CentralNode = DeviceProxy(central_node)
     CentralNode.ReleaseResources(release_input_str)
     LOGGER.info(
-            f"ReleaseResources command is invoked on {centralnode}"
+            f"ReleaseResources command is invoked on {central_node}"
     )
