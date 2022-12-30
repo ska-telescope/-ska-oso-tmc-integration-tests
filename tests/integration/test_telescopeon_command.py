86b769f1c28ead9c6582a582b269c4362efb8781
Bhushan Parakh <bhushan.ska@gmail.com>
2022-12-30 17:22:00 +0530

    HM-128: Modified test case

--- tests/integration/test_telescopeon_command.py
+++ tests/integration/test_telescopeon_command.py
@@ -2,6 +2,7 @@ import pytest
 from tests.resources.test_support.controls import telescope_is_in_standby_state, telescope_is_in_on_state, telescope_is_in_off_state
 import tests.resources.test_support.tmc_helpers as tmc
 from tests.conftest import LOGGER
+from tango import DeviceProxy
 
 @pytest.mark.SKA_mid
 def test_telescope_on():
@@ -44,4 +45,6 @@ def test_telescope_on():
 def test_telescope_on():
     """TelescopeOn() is executed."""
     LOGGER.info("Placeholder test case for low deployment")
-    assert 1
+    sdp_low = DeviceProxy("low-sdp/subarray/01")
+    ping = sdp_low.ping()
+    assert ping > 1
