1d68709415faf361b7089a59bfc6c3f0abd58ecb
yogeshk.ska <yogeshk.ska@gmail.com>
2023-01-02 18:41:11 +0530

    HM-127: commented out code removed

--- tests/resources/test_support/low/controls.py
+++ tests/resources/test_support/low/controls.py
@@ -7,35 +7,9 @@ from tests.conftest import LOGGER
 from tests.resources.test_support.constant_low import *
 
 # Tango device fqdns used across to create device proxy
-    
-def validate_states(device_info, attr):
-    """_summary_
-
-    Args:
-        device_info (_type_): _description_
-    """
-    device_names = device_info.keys()
-    state_validations = []
-    for dev_name in device_names:
-        LOGGER.info(f'resource.get("{attr}")' + str(resource(dev_name).get(attr)))
-        state_to_check = device_info[dev_name]
-        state_validations.append(
-            resource(dev_name).get(attr) in state_to_check
-        )
-    return state_to_check
 
 
 def telescope_is_in_standby_state():
-    # device_info = {
-    #     sdp_master: ["DISABLE", "STANDBY"],
-    #     sdp_subarray1: ["DISABLE" , "OFF"], 
-    #     csp_master: ["DISABLE", "STANDBY"], 
-    #     csp_subarray1: ["DISABLE", "OFF"]
-    # }
-    # if TELESCOPE_ENV == "SKA-mid":
-    #     device_info[dish_master1] = ["DISABLE", "OFF"]
-    
-    # return validate_states(device_info, "State")
     LOGGER.info(
         'resource(sdp_master).get("State")'
         + str(resource(sdp_master).get("State"))
@@ -65,17 +39,6 @@ def telescope_is_in_on_state():
         'resource(tmc_subarraynode1).get("State")'
         + str(resource(tmc_subarraynode1).get("State"))
     )
-    # device_info = {
-    #     sdp_master: ["ON"],
-    #     sdp_subarray1: ["ON"], 
-    #     csp_master: ["ON"], 
-    #     csp_subarray1: ["ON"],
-    #     centralnode: ["ON"]
-    # }
-    # if TELESCOPE_ENV == "SKA-mid":
-    #     device_info[dish_master1] = ["ON"]
-    
-    # return validate_states(device_info, "State")
     LOGGER.info(
         'resource(sdp_master).get("State")'
         + str(resource(sdp_master).get("State"))
