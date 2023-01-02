2278759ecadfdd4f2a7631d27f8022339944cf31
Bhushan Parakh <bhushan.ska@gmail.com>
2023-01-02 12:36:50 +0530

    HM-128: Resolved review comment

--- tests/integration/test_telescopeon_command.py
+++ tests/integration/test_telescopeon_command.py
@@ -41,7 +41,8 @@ def test_telescope_on():
         raise
 
 @pytest.mark.SKA_low
-def test_sample():
+def test_device_running():
+    """Tests whether the Central Node Low is up."""
     proxy = DeviceProxy("ska_low/tm_central/central_node")
     result = proxy.ping()
     assert result > 1
