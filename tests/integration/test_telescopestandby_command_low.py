38bdd5621a15bfc99e477b89a7992d2bc61b63c4
yogeshk.ska <yogeshk.ska@gmail.com>
2023-01-04 12:57:11 +0530

    HM-126: standby command added

---
+++ tests/integration/test_telescopestandby_command_low.py
@@ -0,0 +1,41 @@
+import pytest
+from tests.resources.test_support.low.controls import telescope_is_in_standby_state, telescope_is_in_on_state, telescope_is_in_off_state
+import tests.resources.test_support.low.tmc_helpers as tmc
+from tests.conftest import LOGGER
+
+@pytest.mark.SKA_low
+def test_telescope_standby():
+    """TelescopeStandby() is executed."""
+    try:
+        fixture = {}
+        fixture["state"] = "Unknown"
+
+        """Verify Telescope is Off/Standby"""
+        assert telescope_is_in_standby_state()
+        LOGGER.info("Staring up the Telescope")
+
+        """Invoke TelescopeOn() command on TMC"""
+        LOGGER.info("Invoking TelescopeOn command on TMC CentralNode")
+        tmc.set_to_on()
+        LOGGER.info("TelescopeOn command is invoked successfully")
+
+        """Verify State transitions after TelescopeOn"""
+        assert telescope_is_in_on_state()
+        fixture["state"] = "TelescopeOn"
+
+        """Invoke TelescopeOff() command on TMC"""
+        tmc.set_to_standby()
+
+        """Verify State transitions after TelescopeOff"""
+        assert telescope_is_in_standby_state()
+        fixture["state"] = "TelescopeOff"
+
+        LOGGER.info("Tests complete.")
+        
+
+    except:
+        LOGGER.info("Exception occurred in the test for state = {}".format(fixture["state"]))
+        LOGGER.info("Tearing down...")
+        if fixture["state"] == "TelescopeOn":
+            tmc.set_to_off()
+        raise
