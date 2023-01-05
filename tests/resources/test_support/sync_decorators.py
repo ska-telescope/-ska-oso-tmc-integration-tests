79f64752c3283b5ad71a6dd0fb3574e3e7db7216
akshaysonawane <akshayss.ska@gmail.com>
2023-01-05 10:52:02 +0530

    HM-125: Update wait

--- tests/resources/test_support/sync_decorators.py
+++ tests/resources/test_support/sync_decorators.py
@@ -31,7 +31,7 @@ def sync_telescope_on(func):
         the_waiter = waiter()
         the_waiter.set_wait_for_telescope_on()
         result = func(*args, **kwargs)
-        the_waiter.wait(200)
+        the_waiter.wait(210)
         return result
 
     return wrapper
@@ -87,7 +87,7 @@ def sync_assign_resources():
             the_waiter = waiter()
             the_waiter.set_wait_for_assign_resources()
             result = func(*args, **kwargs)
-            the_waiter.wait(210)
+            the_waiter.wait(200)
             return result
 
         return wrapper
