c79b6376d4fbc3ea93c13010ba8d6848eeacc2f1
akshaysonawane <akshayss.ska@gmail.com>
2023-02-14 17:35:24 +0530

    HM-165: Resolve review comments

--- tests/integration/test_off_not_allowed_in_ready.py
+++ tests/integration/test_off_not_allowed_in_ready.py
@@ -10,8 +10,9 @@ assign_resources_file = "command_AssignResources.json"
 release_resources_file  = "command_ReleaseResources.json"
 
 @pytest.mark.SKA_mid
-@pytest.mark.akii
-def test_scan_not_allowed_in_idle():  
+@pytest.mark.skip(reason="The test is xfailed as there is no exception raised for off command when called in idle state")
+def test_off_not_allowed_in_idle():  
+
     # try: 
     fixture = {}
     fixture["state"] = "Unknown"
@@ -37,19 +38,24 @@ def test_scan_not_allowed_in_idle():
     """Verify ObsState is Idle"""
     # Given a Subarray in IDLE observation state
     assert subarray_obs_state_is_idle()
+    # tmc.set_to_off()
     fixture["state"] ="AssignResources"
-    scan_input = tmc.get_input_str(scan_file)            
     subarray_node = DeviceProxy("ska_mid/tm_subarray_node/1")
+    # subarray_node.Off()
+    # scan_input = tmc.get_input_str(scan_file)            
+    # central_node = DeviceProxy("ska_mid/tm_central/central_node")
+    # central_node.TelescopeOff()
     with pytest.raises(Exception) as info:
-    # When SCAN command invoked
-        subarray_node.Scan(scan_input)
-    # Then it fails with Command not Allowed error
-    assert "Scan command not permitted in observation state IDLE" in str(info.value)
+    # # When OFF command invoked
+        subarray_node.Off()
+    # # Then it fails with Command not Allowed error
+    assert "OFF command not permitted in observation state IDLE" in str(info.value)
     # And TMC remains in IDLE observation state
     assert subarray_obs_state_is_idle()
     """Invoke ReleaseResources() command on TMC"""
     release_input_str = tmc.get_input_str(release_resources_file)
     tmc.invoke_releaseResources(release_input_str)
+
     fixture["state"] = "ReleaseResources"
     assert subarray_obs_state_is_empty()
 
@@ -59,10 +65,3 @@ def test_scan_not_allowed_in_idle():
     """Verify State transitions after TelescopeOff"""
     assert telescope_is_in_off_state()
     fixture["state"] = "TelescopeOff"
-
-    # except:
-    #     if fixture["state"] == "AssignResources":
-    #         tmc.invoke_releaseResources(release_input_str)
-    #     if fixture["state"] == "TelescopeOn":
-    #         tmc.set_to_off()
-    #     raise
