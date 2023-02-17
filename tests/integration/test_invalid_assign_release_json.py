23e1ba305f8362f6a38e8b29e4f3c8f23eca4f0e
akshaysonawane <akshayss.ska@gmail.com>
2023-02-17 21:44:29 +0530

    HM-165: fix test cases

--- tests/integration/test_invalid_assign_release_json.py
+++ tests/integration/test_invalid_assign_release_json.py
@@ -12,115 +12,136 @@ from tests.resources.test_support.constant import (
 @pytest.mark.aki
 @pytest.mark.SKA_mid
 def test_assign_invalid_json(json_factory):
-    """AssignResources and ReleaseResources is executed."""
-    assign_json = json_factory("command_invalid_assign_release")
-    tmc.check_devices()
-    fixture = {}
-    fixture["state"] = "Unknown"
+    try:
+        """AssignResources and ReleaseResources is executed."""
+        assign_json = json_factory("command_invalid_assign_release")
+        tmc.check_devices()
+        fixture = {}
+        fixture["state"] = "Unknown"
 
-    """Verify Telescope is Off/Standby"""
-    assert telescope_is_in_standby_state()
-    LOGGER.info("Staring up the Telescope")
+        """Verify Telescope is Off/Standby"""
+        assert telescope_is_in_standby_state()
+        LOGGER.info("Staring up the Telescope")
 
-    """Invoke TelescopeOn() command on TMC"""
-    LOGGER.info("Invoking TelescopeOn command on TMC CentralNode")
-    tmc.set_to_on()
-    LOGGER.info("TelescopeOn command is invoked successfully")
+        """Invoke TelescopeOn() command on TMC"""
+        LOGGER.info("Invoking TelescopeOn command on TMC CentralNode")
+        tmc.set_to_on()
+        LOGGER.info("TelescopeOn command is invoked successfully")
 
-    """Verify State transitions after TelescopeOn"""
-    assert telescope_is_in_on_state()
-    fixture["state"] = "TelescopeOn"
+        """Verify State transitions after TelescopeOn"""
+        assert telescope_is_in_on_state()
+        fixture["state"] = "TelescopeOn"
 
-    """Invoke AssignResources() Command on TMC"""
-    LOGGER.info("Invoking AssignResources command on TMC CentralNode")
-    resource("ska_mid/tm_subarray_node/1").assert_attribute("State").equals(
-        "ON"
-    )
-    resource("ska_mid/tm_subarray_node/1").assert_attribute("obsState").equals(
-        "EMPTY"
-    )            
-    central_node = DeviceProxy("ska_mid/tm_central/central_node")
-    tmc.check_devices()
-    ret_code, message = central_node.AssignResources(assign_json)
+        """Invoke AssignResources() Command on TMC"""
+        LOGGER.info("Invoking AssignResources command on TMC CentralNode")
+        resource("ska_mid/tm_subarray_node/1").assert_attribute("State").equals(
+            "ON"
+        )
+        resource("ska_mid/tm_subarray_node/1").assert_attribute("obsState").equals(
+            "EMPTY"
+        )            
+        central_node = DeviceProxy("ska_mid/tm_central/central_node")
+        tmc.check_devices()
+        ret_code, message = central_node.AssignResources(assign_json)
 
-   #Assert with TaskStatus as REJECTED
-    assert ret_code == 5
-    LOGGER.info(message)
+        #Assert with TaskStatus as REJECTED
+        assert ret_code == 5
+        LOGGER.info(message)
 
-    """Verify ObsState is Idle"""
-    assert telescope_is_in_on_state()
+        """Verify ObsState is Idle"""
+        assert telescope_is_in_on_state()
 
-    # """Invoke TelescopeOff() command on TMC"""
-    tmc.set_to_off()
+        # """Invoke TelescopeOff() command on TMC"""
+        tmc.set_to_off()
 
-    # """Verify State transitions after TelescopeOff"""
-    assert telescope_is_in_off_state()
-    fixture["state"] = "TelescopeOff"
+        # """Verify State transitions after TelescopeOff"""
+        assert telescope_is_in_off_state()
+        fixture["state"] = "TelescopeOff"
 
-    # LOGGER.info("Tests complete.")
+        # LOGGER.info("Tests complete.")
+    except:
+        LOGGER.info("Exception occurred in the test for state = {}".format(fixture["state"]))
+        LOGGER.info("Tearing down...")
+        if fixture["state"] == "TelescopeOn":
+            tmc.set_to_off()
+        raise
 
 @pytest.mark.akii
 @pytest.mark.SKA_mid
 def test_release_invalid_json(json_factory):
-    """AssignResources and ReleaseResources is executed."""
+    try:
+        """AssignResources and ReleaseResources is executed."""
 
-    assign_json = json_factory("command_AssignResources")
-    release_json = json_factory("command_invalid_assign_release")
-    tmc.check_devices()
-    fixture = {}
-    fixture["state"] = "Unknown"
-
-    """Verify Telescope is Off/Standby"""
-    assert telescope_is_in_standby_state()
-    LOGGER.info("Staring up the Telescope")
-
-    """Invoke TelescopeOn() command on TMC"""
-    LOGGER.info("Invoking TelescopeOn command on TMC CentralNode")
-    tmc.set_to_on()
-    LOGGER.info("TelescopeOn command is invoked successfully")
-
-    """Verify State transitions after TelescopeOn"""
-    assert telescope_is_in_on_state()
-    fixture["state"] = "TelescopeOn"
-
-    """Invoke AssignResources() Command on TMC"""
-    LOGGER.info("Invoking AssignResources command on TMC CentralNode")
-    @sync_assign_resources()
-    def compose_sub():
-        resource(tmc_subarraynode1).assert_attribute("State").equals(
-            "ON"
-        )
-        resource(tmc_subarraynode1).assert_attribute("obsState").equals(
-            "EMPTY"
-        )
-        central_node = DeviceProxy(centralnode)
+        assign_json = json_factory("command_AssignResources")
+        release_json = json_factory("command_invalid_assign_release")
         tmc.check_devices()
-        central_node.AssignResources(assign_json)
-        LOGGER.info("Invoked AssignResources on CentralNode")
+        fixture = {}
+        fixture["state"] = "Unknown"
 
-    compose_sub()
+        """Verify Telescope is Off/Standby"""
+        assert telescope_is_in_standby_state()
+        LOGGER.info("Staring up the Telescope")
 
-    LOGGER.info("AssignResources command is invoked successfully")
+        """Invoke TelescopeOn() command on TMC"""
+        LOGGER.info("Invoking TelescopeOn command on TMC CentralNode")
+        tmc.set_to_on()
+        LOGGER.info("TelescopeOn command is invoked successfully")
 
-    """Verify ObsState is Idle"""
-    assert subarray_obs_state_is_idle()
-    fixture["state"] ="AssignResources"
-    
-    """Invoke ReleaseResources() command on TMC"""
-    central_node = DeviceProxy(centralnode)
-    ret_code, message=central_node.ReleaseResources(release_json)
+        """Verify State transitions after TelescopeOn"""
+        assert telescope_is_in_on_state()
+        fixture["state"] = "TelescopeOn"
 
-    assert ret_code == 5
-    LOGGER.info(message)
+        """Invoke AssignResources() Command on TMC"""
+        LOGGER.info("Invoking AssignResources command on TMC CentralNode")
+        @sync_assign_resources()
+        def compose_sub():
+            resource(tmc_subarraynode1).assert_attribute("State").equals(
+                "ON"
+            )
+            resource(tmc_subarraynode1).assert_attribute("obsState").equals(
+                "EMPTY"
+            )
+            central_node = DeviceProxy(centralnode)
+            tmc.check_devices()
+            central_node.AssignResources(assign_json)
+            LOGGER.info("Invoked AssignResources on CentralNode")
 
-    fixture["state"] = "ReleaseResources"
-    assert subarray_obs_state_is_idle()
+        compose_sub()
 
-    # """Invoke TelescopeOff() command on TMC"""
-    # tmc.set_to_off()
+        LOGGER.info("AssignResources command is invoked successfully")
 
-    # """Verify State transitions after TelescopeOff"""
-    # assert telescope_is_in_off_state()
-    # fixture["state"] = "TelescopeOff"
+        """Verify ObsState is Idle"""
+        assert subarray_obs_state_is_idle()
+        fixture["state"] ="AssignResources"
+        
+        """Invoke ReleaseResources() command on TMC"""
+        central_node = DeviceProxy(centralnode)
+        ret_code, message=central_node.ReleaseResources(release_json)
+        #Assert with TaskStatus as REJECTED
+        assert ret_code == 5
+        LOGGER.info(message)
+        # Check if telescope is in previous state
+        fixture["state"] = "ReleaseResources"
+        assert subarray_obs_state_is_idle()
+        #Invoke release resources
+        """Invoke ReleaseResources() command on TMC"""
+        tmc.invoke_releaseResources(release_json)
 
-    # LOGGER.info("Tests complete.")
+        fixture["state"] = "ReleaseResources"
+        assert subarray_obs_state_is_empty()
+
+        """Invoke TelescopeOff() command on TMC"""
+        tmc.set_to_off()
+
+        """Verify State transitions after TelescopeOff"""
+        assert telescope_is_in_off_state()
+        fixture["state"] = "TelescopeOff"
+
+        LOGGER.info("Tests complete.")
+
+    except:
+        if fixture["state"] == "AssignResources":
+            tmc.invoke_releaseResources(release_json)
+        if fixture["state"] == "TelescopeOn":
+            tmc.set_to_off()
+        raise
