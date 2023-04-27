1e826059c38e9dd8af4ad07a7417b5d7102c6060
manthan.ska <manthan.ska@gmail.com>
2023-04-27 14:17:47 +0530

    SAH-1316: Testing test module

--- tests/integration/check_command_allowed/test_command_not_allowed_idle.py
+++ tests/integration/check_command_allowed/test_command_not_allowed_idle.py
@@ -23,7 +23,7 @@ tmc_helper = TmcHelper(centralnode, tmc_subarraynode1)
 telescope_control = TelescopeControlMid()
 
 
-
+@pytest.mark.m
 @pytest.mark.SKA_mid
 @scenario(
     "../features/check_command_not_allowed.feature",
