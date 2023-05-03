4dc19fa7b5134a8515b818b28a5bad350c401263
mansi.shirore <mansis.ska@gmail.com>
2023-05-03 17:52:02 +0530

    SAH-1323: Address review comments.

--- tests/features/invalid_json_not_allowed.feature
+++ tests/features/invalid_json_not_allowed.feature
@@ -1,15 +1,17 @@
 Feature: Commands with invalid json input
     Scenario: AssignResource command with invalid JSON is rejected by the TMC 
-        Given the TMC is in ON state and the subarray is in EMPTY obsState
+        Given the TMC is in ON state 
+        And the subarray is in EMPTY obsState
         When the command AssignResources is invoked with <invalid_json> input
         Then TMC should reject the AssignResources command
         And TMC subarray remains in EMPTY obsState
         And the command AssignResources is invoked with valid_json input
         Examples:
-            | invalid_json                             |
-            | invalid_assign_json_without_pb_id        |
-            | invalid_assign_json_without_scan_type_id |
-            | invalid_assign_json_without_channel_keys |
-            | invalid_assign_json_without_receiptor_id |
+            | invalid_json      |
+            | pb_id             |
+            | scan_type_id      |
+            | count             |
+            | receptor_id       |
+
             
 
--- tests/integration/command_not_allowed_with_invalid_input/test_assign_not_allowed_with_invalid_json.py
+++ tests/integration/command_not_allowed_with_invalid_input/test_assign_not_allowed_with_invalid_json.py
@@ -1,3 +1,5 @@
+import json
+
 import pytest
 from pytest_bdd import given, parsers, scenario, then, when
 
@@ -20,6 +22,7 @@ from tests.resources.test_support.tmc_helpers import tear_down
 
 tmc_helper = TmcHelper(centralnode, tmc_subarraynode1)
 telescope_control = BaseTelescopeControl()
+assign_resources_file = "command_AssignResources"
 
 
 @pytest.mark.xfail(reason="This functionality is not implemented yet in TMC")
@@ -35,21 +38,22 @@ def test_assign_resource_with_invalid_json():
     """
 
 
-@given("the TMC is in ON state and the subarray is in EMPTY obsState")
+@given("the TMC is in ON state")
 def given_tmc():
     # Verify Telescope is Off/Standby
     assert telescope_control.is_in_valid_state(
         DEVICE_STATE_STANDBY_INFO, "State"
     )
-    LOGGER.info("Starting up the Telescope")
-
     # Invoke TelescopeOn() command on TMC CentralNode
     LOGGER.info("Invoking TelescopeOn command on TMC CentralNode")
     tmc_helper.set_to_on(**ON_OFF_DEVICE_COMMAND_DICT)
     LOGGER.info("TelescopeOn command is invoked successfully")
-
     # Verify State transitions after TelescopeOn
     assert telescope_control.is_in_valid_state(DEVICE_STATE_ON_INFO, "State")
+
+
+@given("the subarray is in EMPTY obsState")
+def given_tmc_obsstate():
     assert telescope_control.is_in_valid_state(
         DEVICE_OBS_STATE_EMPTY_INFO, "obsState"
     )
@@ -62,14 +66,43 @@ def given_tmc():
 )
 def send(json_factory, invalid_json):
     try:
-        assign_invalid_json = json_factory(invalid_json)
-        # Invoke AssignResources() Command on TMC
-        LOGGER.info("Invoking AssignResources command on TMC CentralNode")
-        pytest.command_result = tmc_helper.compose_sub(
-            assign_invalid_json, **ON_OFF_DEVICE_COMMAND_DICT
-        )
+        assign_invalid_json = json_factory(assign_resources_file)
+        assign_invalid_json = json.loads(assign_invalid_json)
+        if invalid_json == "pb_id":
+            del assign_invalid_json["sdp"]["processing_blocks"][0]["pb_id"]
+            # Invoke AssignResources() Command on TMC
+            LOGGER.info("Invoking AssignResources command on TMC CentralNode")
+            pytest.command_result = tmc_helper.compose_sub(
+                json.dumps(assign_invalid_json), **ON_OFF_DEVICE_COMMAND_DICT
+            )
+        elif invalid_json == "scan_type_id":
+            del assign_invalid_json["sdp"]["execution_block"]["scan_types"][0][
+                "scan_type_id"
+            ]
+            # Invoke AssignResources() Command on TMC
+            LOGGER.info("Invoking AssignResources command on TMC CentralNode")
+            pytest.command_result = tmc_helper.compose_sub(
+                json.dumps(assign_invalid_json), **ON_OFF_DEVICE_COMMAND_DICT
+            )
+        elif invalid_json == "count":
+            del assign_invalid_json["sdp"]["execution_block"]["channels"][0][
+                "channels_id"
+            ]
+            # Invoke AssignResources() Command on TMC
+            LOGGER.info("Invoking AssignResources command on TMC CentralNode")
+            pytest.command_result = tmc_helper.compose_sub(
+                json.dumps(assign_invalid_json), **ON_OFF_DEVICE_COMMAND_DICT
+            )
+        elif invalid_json == "receptor_id ":
+            del assign_invalid_json["dish"]["receptor_id"]
+            # Invoke AssignResources() Command on TMC
+            LOGGER.info("Invoking AssignResources command on TMC CentralNode")
+            pytest.command_result = tmc_helper.compose_sub(
+                json.dumps(assign_invalid_json), **ON_OFF_DEVICE_COMMAND_DICT
+            )
+
     except Exception as e:
-        LOGGER.info(f"Exception occured: {e}")
+        LOGGER.exception(f"Exception occured: {e}")
 
 
 # TODO: Current version of TMC does not support ResultCode.REJECTED,
@@ -85,6 +118,7 @@ def invalid_command_rejection():
     )
 
     assert pytest.command_result[0][0] == ResultCode.REJECTED
+    pass
 
 
 @then("TMC subarray remains in EMPTY obsState")
