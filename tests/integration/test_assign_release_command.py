f14260befe04c2592558aec046230229a1923b6f
akshaysonawane <akshayss.ska@gmail.com>
2023-05-25 09:04:53 +0530

    HM-228: add sdp test case

--- tests/integration/test_assign_release_command.py
+++ tests/integration/test_assign_release_command.py
@@ -14,6 +14,8 @@ from tests.resources.test_support.constant import (
     ON_OFF_DEVICE_COMMAND_DICT,
     centralnode,
     csp_subarray1,
+    sdp_subarray1,
+    tmc_sdp_subarray_leaf_node,
     tmc_csp_subarray_leaf_node,
     tmc_subarraynode1,
 )
@@ -167,6 +169,81 @@ def test_assign_release_timeout(json_factory, change_event_callbacks):
         LOGGER.info(f"Exception occurred {e}")
         tear_down(release_json)
 
+
+@pytest.mark.aki
+@pytest.mark.SKA_mid
+def test_assign_release_timeout_sdp(json_factory, change_event_callbacks):
+    """Verify timeout exception raised when sdpp set to defective."""
+    assign_json = json_factory("command_AssignResources")
+    release_json = json_factory("command_ReleaseResources")
+    try:
+        telescope_control = TelescopeControlMid()
+        tmc_helper = TmcHelper(centralnode, tmc_subarraynode1)
+
+        fixture = {}
+        fixture["state"] = "Unknown"
+
+        # Verify Telescope is Off/Standby
+        assert telescope_control.is_in_valid_state(
+            DEVICE_STATE_STANDBY_INFO, "State"
+        )
+        LOGGER.info("Staring up the Telescope")
+
+        # Invoke TelescopeOn() command on TMC
+        LOGGER.info("Invoking TelescopeOn command on TMC CentralNode")
+        tmc_helper.set_to_on(**ON_OFF_DEVICE_COMMAND_DICT)
+        LOGGER.info("TelescopeOn command is invoked successfully")
+
+        # Verify State transitions after TelescopeOn
+        assert telescope_control.is_in_valid_state(
+            DEVICE_STATE_ON_INFO, "State"
+        )
+        fixture["state"] = "TelescopeOn"
+
+        # Invoke AssignResources() Command on TMC
+        LOGGER.info("Invoking AssignResources command on TMC CentralNode")
+        # Verify State transitions after TelescopeOn
+
+        central_node = DeviceProxy(centralnode)
+        central_node.subscribe_event(
+            "longRunningCommandResult",
+            EventType.CHANGE_EVENT,
+            change_event_callbacks["longRunningCommandResult"],
+        )
+
+        sdp_subarray = DeviceProxy(sdp_subarray1)
+        sdp_subarray.SetDefective(True)
+
+        device_params = deepcopy(ON_OFF_DEVICE_COMMAND_DICT)
+        device_params["set_wait_for_finish"] = False
+        unique_id, result = tmc_helper.compose_sub(
+            assign_json, **device_params
+        )
+
+        LOGGER.info(f"Command result {result} and unique id {unique_id}")
+
+        assert result[0].endswith("AssignResources")
+        assert unique_id[0] == ResultCode.QUEUED
+
+        exception_message = (
+            f"Exception occured on device: "
+            f"{tmc_subarraynode1}: Exception occured on device"
+            f": {tmc_sdp_subarray_leaf_node}: Timeout has "
+            f"occured, command failed"
+        )
+
+        change_event_callbacks["longRunningCommandResult"].assert_change_event(
+            (result[0], exception_message),
+            lookahead=7,
+        )
+        sdp_subarray.SetDefective(False)
+
+        # Do not raise exception
+        tear_down(release_json, raise_exception=False)
+
+    except Exception as e:
+        LOGGER.info(f"Exception occurred {e}")
+        tear_down(release_json)
 
 @pytest.mark.SKA_mid
 def test_health_check_mid():
