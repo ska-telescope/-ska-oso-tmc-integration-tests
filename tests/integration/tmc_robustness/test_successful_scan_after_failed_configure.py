117cb071413cfcfd63541a5d64e2b3faa7c061b0
manthan.ska <manthan.ska@gmail.com>
2023-05-25 17:57:32 +0530

    SAH-1334: Removed unwanted mark

--- tests/integration/tmc_robustness/test_successful_scan_after_failed_configure.py
+++ tests/integration/tmc_robustness/test_successful_scan_after_failed_configure.py
@@ -31,7 +31,6 @@ tmc_helper = TmcHelper(centralnode, tmc_subarraynode1)
 telescope_control = BaseTelescopeControl()
 
 
-@pytest.mark.test1
 @pytest.mark.SKA_mid
 @scenario(
     "../features/successful_scan_after_failed_configure.feature",
@@ -201,7 +200,6 @@ def teardown_the_tmc(json_factory):
     )
 
 
-@pytest.mark.test1
 @pytest.mark.SKA_mid
 @scenario(
     "../features/successful_scan_after_failed_configure.feature",
