0d9a7daf78fcda5e59c94d827231936b58763c78
mansi.shirore <mansis.ska@gmail.com>
2023-05-30 12:31:35 +0530

    SAH-1331: Added test case for unavailibilty of device.

--- tests/integration/test_assign_release_unavailability.py
+++ tests/integration/test_assign_release_unavailability.py
@@ -3,31 +3,20 @@ from tango import DeviceProxy
 
 import tests.resources.test_support.tmc_helpers as tmc
 from tests.conftest import LOGGER
-from tests.resources.test_support.constant import (
-    DEVICE_HEALTH_STATE_OK_INFO,
-    centralnode,
-    tmc_subarraynode1,
-)
+from tests.resources.test_support.constant import tmc_subarraynode1
 from tests.resources.test_support.controls import (
-    subarray_obs_state_is_empty,
-    subarray_obs_state_is_idle,
     telescope_is_in_on_state,
     telescope_is_in_standby_state,
 )
-from tests.resources.test_support.helpers import resource
 from tests.resources.test_support.exception import CommandNotAllowed
-from tests.resources.test_support.sync_decorators import sync_assign_resources
-from tests.resources.test_support.telescope_controls import (
-    BaseTelescopeControl,
-)
-from tests.resources.test_support.tmc_helpers import tear_down
+
 
 @pytest.mark.skip
 @pytest.mark.SKA_mid
 def test_assign_release(json_factory):
     """AssignResources and ReleaseResources is executed."""
     assign_json = json_factory("command_AssignResources")
-    release_json = json_factory("command_ReleaseResources")
+    # release_json = json_factory("command_ReleaseResources")
     try:
         # Verify Telescope is Off/Standby
         assert telescope_is_in_standby_state()
@@ -56,6 +45,3 @@ def test_assign_release(json_factory):
 
     except Exception as e:
         LOGGER.info(f"Exception raise:{e}")
-
-
-
