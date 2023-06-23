5b97ae2f63d76e8e5a6b1b2a5d70d0f690fa5579
yogeshk.ska <yogeshk.ska@gmail.com>
2023-06-23 11:44:27 +0530

    HM-232: lint issue solved

--- tests/integration/tmc_harness/test_assign.py
+++ tests/integration/tmc_harness/test_assign.py
@@ -1,7 +1,8 @@
 # TODO: WIP
 # import pytest
 
-# from tests.resources.test_harness.helpers import check_obs_state, check_state
+# from tests.resources.test_harness.helpers import check_obs_state,
+# check_state
 # from tests.resources.test_support.tmc_helpers import tear_down
 
 
@@ -9,7 +10,8 @@
 # @pytest.mark.SKA_Mid
 # def test_assign_resources(central_node, json_factory):
 #     """
-#     Test module to verify AssignResources command, obsState transitions from EMPTY to IDLE
+#     Test module to verify AssignResources command, obsState transitions
+#     from EMPTY to IDLE
 #     """
 #     assign_json = json_factory("command_AssignResources")
 #     release_json = json_factory("command_ReleaseResources")
