059f857bdfab22f15f29de1fdc12aae924d3a233
yogeshk.ska <yogeshk.ska@gmail.com>
2023-06-26 19:06:23 +0530

    HM-232: tear down update

--- tests/resources/test_harness/utils/enums.py
+++ tests/resources/test_harness/utils/enums.py
@@ -6,7 +6,8 @@ from enum import IntEnum, unique
 @unique
 class DishMode(IntEnum):
     # ska-mid-dish-manager is having dependency conflicts with ska-tmc-common
-    # So redefined DishMode enum, which reflects the ska-mid-dish-manager DishMode enum.
+    # So redefined DishMode enum, which reflects the ska-mid-dish-manager
+    # DishMode enum.
     # We will work out on this separately once dish manager is sorted.
     STARTUP = 0
     SHUTDOWN = 1
