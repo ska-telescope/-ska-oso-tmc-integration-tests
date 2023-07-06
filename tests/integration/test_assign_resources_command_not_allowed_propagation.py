c32406a9fcd0ed58e686addbbdba85778df06479
Bhushan Parakh <bhushan.ska@gmail.com>
2023-07-06 12:16:32 +0530

    HM-250: Adding test case for command not allowed error propagation.

---
+++ tests/integration/test_assign_resources_command_not_allowed_propagation.py
@@ -0,0 +1,95 @@
+from copy import deepcopy
+
+import pytest
+from ska_tango_testing.mock.placeholders import Anything
+from tango import DeviceProxy, EventType
+
+from tests.conftest import LOGGER
+from tests.resources.test_support.common_utils.result_code import ResultCode
+from tests.resources.test_support.common_utils.tmc_helpers import TmcHelper
+from tests.resources.test_support.constant import (
+    DEVICE_STATE_ON_INFO,
+    DEVICE_STATE_STANDBY_INFO,
+    ON_OFF_DEVICE_COMMAND_DICT,
+    centralnode,
+    csp_subarray1,
+    tmc_subarraynode1,
+)
+from tests.resources.test_support.mid.telescope_controls_mid import (
+    TelescopeControlMid,
+)
+from tests.resources.test_support.tmc_helpers import tear_down
+
+
+@pytest.mark.skip(
+    reason="Abort command is not implemented on SDP Subarray Leaf Node. \
+        The functionality is verified and will work."
+)
+@pytest.mark.SKA_mid
+def test_assign_release_command_not_allowed_propagation(
+    json_factory, change_event_callbacks
+):
+    """Verify timeout exception raised when csp set to defective."""
+    assign_json = json_factory("command_AssignResources")
+    release_json = json_factory("command_ReleaseResources")
+    try:
+        telescope_control = TelescopeControlMid()
+        tmc_helper = TmcHelper(centralnode, tmc_subarraynode1)
+
+        # Verify Telescope is Off/Standby
+        assert telescope_control.is_in_valid_state(
+            DEVICE_STATE_STANDBY_INFO, "State"
+        )
+
+        # Invoke TelescopeOn() command on TMC
+        tmc_helper.set_to_on(**ON_OFF_DEVICE_COMMAND_DICT)
+
+        # Verify State transitions after TelescopeOn
+        assert telescope_control.is_in_valid_state(
+            DEVICE_STATE_ON_INFO, "State"
+        )
+
+        # Invoke AssignResources() Command on TMC
+        LOGGER.info("Invoking AssignResources command on TMC CentralNode")
+        # Verify State transitions after TelescopeOn
+
+        central_node = DeviceProxy(centralnode)
+        central_node.subscribe_event(
+            "longRunningCommandResult",
+            EventType.CHANGE_EVENT,
+            change_event_callbacks["longRunningCommandResult"],
+        )
+
+        csp_subarray = DeviceProxy(csp_subarray1)
+        # Setting CSP Subarray ObsState to RESOURCING to imulate failure.
+        csp_subarray.SetDirectObsState(1)
+
+        device_params = deepcopy(ON_OFF_DEVICE_COMMAND_DICT)
+        device_params["set_wait_for_obsstate"] = False
+        result, unique_id = tmc_helper.compose_sub(
+            assign_json, **device_params
+        )
+
+        LOGGER.info(f"Command result {result} and unique id {unique_id}")
+
+        assert unique_id[0].endswith("AssignResources")
+        assert result[0] == ResultCode.QUEUED
+
+        assertion_data = change_event_callbacks[
+            "longRunningCommandResult"
+        ].assert_change_event(
+            (unique_id[0], Anything),
+            lookahead=7,
+        )
+        assert "AssignResources" in assertion_data["attribute_value"][0]
+        assert (
+            "ska_tmc_common.exceptions.InvalidObsStateError"
+            in assertion_data["attribute_value"][1]
+        )
+
+        # Do not raise exception
+        tear_down(release_json, raise_exception=False)
+
+    except Exception as e:
+        LOGGER.info(f"Exception occurred {e}")
+        tear_down(release_json)
