99af75a9d4377a66afd47f527b995da9f49ba395
devesh pandey <devesh.ska@gmail.com>
2023-07-10 16:19:22 +0530

    HM-254:Update test case

--- tests/integration/test_subarray_stuck_resourcing.py
+++ tests/integration/test_subarray_stuck_resourcing.py
@@ -19,86 +19,79 @@ from tests.resources.test_support.helpers import resource, waiter
 from tests.resources.test_support.telescope_controls import (
     BaseTelescopeControl,
 )
-from tests.resources.test_support.tmc_helpers import tear_down
 
 
 @pytest.mark.SKA_mid
 def test_assign_release(json_factory):
     """AssignResources and ReleaseResources is executed."""
     assign_json = json_factory("command_AssignResources")
-    release_json = json_factory("command_ReleaseResources")
-    try:
-        telescope_control = BaseTelescopeControl()
-        tmc_helper = TmcHelper(centralnode, tmc_subarraynode1)
 
+    telescope_control = BaseTelescopeControl()
+    tmc_helper = TmcHelper(centralnode, tmc_subarraynode1)
+
+    tmc_helper.check_devices(DEVICE_LIST_FOR_CHECK_DEVICES)
+
+    # Verify Telescope is Off/Standby
+    assert telescope_control.is_in_valid_state(
+        DEVICE_STATE_STANDBY_INFO, "State"
+    )
+    LOGGER.info("Staring up the Telescope")
+
+    # Invoke TelescopeOn() command on TMC
+    LOGGER.info("Invoking TelescopeOn command on TMC CentralNode")
+    tmc_helper.set_to_on(**ON_OFF_DEVICE_COMMAND_DICT)
+    LOGGER.info("TelescopeOn command is invoked successfully")
+
+    # Verify State transitions after TelescopeOn
+    # Verify State transitions after TelescopeOn#
+    assert telescope_control.is_in_valid_state(DEVICE_STATE_ON_INFO, "State")
+
+    the_waiter = waiter()
+    # Invoke AssignResources() Command on TMC
+    LOGGER.info("Invoking AssignResources command on TMC CentralNode")
+    sdp_subarray = DeviceProxy(sdp_subarray1)
+
+    def compose_sub():
+        # Added this check to ensure that devices are running to avoid
+        # random test failures.
         tmc_helper.check_devices(DEVICE_LIST_FOR_CHECK_DEVICES)
-
-        # Verify Telescope is Off/Standby
-        assert telescope_control.is_in_valid_state(
-            DEVICE_STATE_STANDBY_INFO, "State"
+        central_node = DeviceProxy(centralnode)
+        sdp_subarray.SetRaiseException(True)
+        resource(tmc_subarraynode1).assert_attribute("State").equals("ON")
+        resource(tmc_subarraynode1).assert_attribute("obsState").equals(
+            "EMPTY"
         )
-        LOGGER.info("Staring up the Telescope")
-
-        # Invoke TelescopeOn() command on TMC
-        LOGGER.info("Invoking TelescopeOn command on TMC CentralNode")
-        tmc_helper.set_to_on(**ON_OFF_DEVICE_COMMAND_DICT)
-        LOGGER.info("TelescopeOn command is invoked successfully")
-
-        # Verify State transitions after TelescopeOn
-        # Verify State transitions after TelescopeOn#
-        assert telescope_control.is_in_valid_state(
-            DEVICE_STATE_ON_INFO, "State"
+        central_node.AssignResources(assign_json)
+        the_waiter.set_wait_for_specific_obsstate(
+            "RESOURCING", [sdp_subarray1, tmc_subarraynode1]
         )
+        the_waiter.set_wait_for_specific_obsstate("IDLE", [csp_subarray1])
+        the_waiter.wait(30)
 
-        the_waiter = waiter()
-        # Invoke AssignResources() Command on TMC
-        LOGGER.info("Invoking AssignResources command on TMC CentralNode")
-        sdp_subarray = DeviceProxy(sdp_subarray1)
+    compose_sub()
 
-        def compose_sub():
-            # Added this check to ensure that devices are running to avoid
-            # random test failures.
-            tmc_helper.check_devices(DEVICE_LIST_FOR_CHECK_DEVICES)
-            central_node = DeviceProxy(centralnode)
-            sdp_subarray.SetRaiseException(True)
-            resource(tmc_subarraynode1).assert_attribute("State").equals("ON")
-            resource(tmc_subarraynode1).assert_attribute("obsState").equals(
-                "EMPTY"
-            )
-            central_node.AssignResources(assign_json)
-            the_waiter.set_wait_for_specific_obsstate(
-                "RESOURCING", [sdp_subarray1, tmc_subarraynode1]
-            )
-            the_waiter.set_wait_for_specific_obsstate("IDLE", [csp_subarray1])
-            the_waiter.wait(30)
+    LOGGER.info("AssignResources command is invoked successfully")
+    sdp_subarray.SetRaiseException(False)
+    sdp_subarray.SetDirectObsState(
+        ObsState.EMPTY
+    )  # as helper don't transition back themselves
+    assert resource(csp_subarray1).get("obsState") == "IDLE"
 
-        compose_sub()
+    assert resource(sdp_subarray1).get("obsState") == "EMPTY"
+    csp_subarray = DeviceProxy(csp_subarray1)
+    csp_subarray.ReleaseAllResources()
+    the_waiter.set_wait_for_specific_obsstate("EMPTY", [csp_subarray1])
 
-        LOGGER.info("AssignResources command is invoked successfully")
-        sdp_subarray.SetRaiseException(False)
-        sdp_subarray.SetDirectObsState(
-            ObsState.EMPTY
-        )  # as helper don't transition back themselves
-        assert resource(csp_subarray1).get("obsState") == "IDLE"
+    assert telescope_control.is_in_valid_state(
+        DEVICE_OBS_STATE_EMPTY_INFO, "obsState"
+    )
 
-        assert resource(sdp_subarray1).get("obsState") == "EMPTY"
-        csp_subarray = DeviceProxy(csp_subarray1)
-        csp_subarray.ReleaseAllResources()
-        the_waiter.set_wait_for_specific_obsstate("EMPTY", [csp_subarray1])
+    # Invoke TelescopeStandby() command on TMC#
+    tmc_helper.set_to_standby(**ON_OFF_DEVICE_COMMAND_DICT)
 
-        assert telescope_control.is_in_valid_state(
-            DEVICE_OBS_STATE_EMPTY_INFO, "obsState"
-        )
+    # Verify State transitions after TelescopeStandby#
+    assert telescope_control.is_in_valid_state(
+        DEVICE_STATE_STANDBY_INFO, "State"
+    )
 
-        # Invoke TelescopeStandby() command on TMC#
-        tmc_helper.set_to_standby(**ON_OFF_DEVICE_COMMAND_DICT)
-
-        # Verify State transitions after TelescopeStandby#
-        assert telescope_control.is_in_valid_state(
-            DEVICE_STATE_STANDBY_INFO, "State"
-        )
-
-        LOGGER.info("Test complete.")
-
-    except Exception:
-        tear_down(release_json)
+    LOGGER.info("Test complete.")
