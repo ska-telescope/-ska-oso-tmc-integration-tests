89a124dd376d88df68f70e9c1dca65d4bc6dd706
Apurva Patkar <apurva.ska@gmail.com>
2023-07-14 09:56:58 +0530

    sah-1356: Add test case for AssignResources command for input JSON with MeerKAT IDs.

--- tests/integration/test_assign_release_command.py
+++ tests/integration/test_assign_release_command.py
@@ -1,3 +1,4 @@
+import json
 from copy import deepcopy
 
 import pytest
@@ -68,6 +69,69 @@ def test_assign_release(json_factory):
             central_node = DeviceProxy(centralnode)
             tmc.check_devices()
             central_node.AssignResources(assign_json)
+            LOGGER.info("Invoked AssignResources on CentralNode")
+
+        compose_sub()
+
+        LOGGER.info("AssignResources command is invoked successfully")
+
+        # Verify ObsState is Idle
+        assert subarray_obs_state_is_idle()
+
+        # Invoke ReleaseResources() command on TMC
+        tmc.invoke_releaseResources(release_json)
+
+        assert subarray_obs_state_is_empty()
+
+        # Invoke TelescopeStandby() command on TMC
+        tmc.set_to_standby()
+
+        # Verify State transitions after TelescopeStandby
+        assert telescope_is_in_standby_state()
+
+        LOGGER.info("Tests complete.")
+
+    except Exception:
+        tear_down(release_json)
+
+
+@pytest.mark.SKA_mid
+def test_assign_release_with_meerkat_ids(json_factory):
+    """AssignResources and ReleaseResources is executed."""
+    assign_json = json_factory("command_AssignResources")
+    # Replace SKA dish ids with MeerKAT dish ids
+    json_argument = json.loads(assign_json)
+    json_argument["dish"]["receptor_ids"] = ["MKT001", "MKT002"]
+    json_argument = json.dumps(json_argument)
+
+    release_json = json_factory("command_ReleaseResources")
+    try:
+        tmc.check_devices()
+
+        # Verify Telescope is Off/Standby
+        assert telescope_is_in_standby_state()
+        LOGGER.info("Staring up the Telescope")
+
+        # Invoke TelescopeOn() command on TMC
+        LOGGER.info("Invoking TelescopeOn command on TMC CentralNode")
+        tmc.set_to_on()
+        LOGGER.info("TelescopeOn command is invoked successfully")
+
+        # Verify State transitions after TelescopeOn
+        assert telescope_is_in_on_state()
+
+        # Invoke AssignResources() Command on TMC
+        LOGGER.info("Invoking AssignResources command on TMC CentralNode")
+
+        @sync_assign_resources()
+        def compose_sub():
+            resource(tmc_subarraynode1).assert_attribute("State").equals("ON")
+            resource(tmc_subarraynode1).assert_attribute("obsState").equals(
+                "EMPTY"
+            )
+            central_node = DeviceProxy(centralnode)
+            tmc.check_devices()
+            central_node.AssignResources(json_argument)
             LOGGER.info("Invoked AssignResources on CentralNode")
 
         compose_sub()
