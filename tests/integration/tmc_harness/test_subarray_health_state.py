b84a7f3cb3d1c72b0d962d7e76a96b64a249c627
yogeshk.ska <yogeshk.ska@gmail.com>
2023-08-04 18:47:10 +0530

    HM-276: event recorder and code review comment addressed

--- tests/integration/tmc_harness/test_subarray_health_state.py
+++ tests/integration/tmc_harness/test_subarray_health_state.py
@@ -9,13 +9,17 @@ from tests.resources.test_harness.helpers import (
 
 class TestSubarrayHealthState(object):
     """This class implement test cases to verify HealthState
-    of Subarray Node"""
+    of Subarray Node.
+    This tests implement rows of following excel sheet
+    https://docs.google.com/spreadsheets/d/1XbNb8We7fK-EhmOcw3S-h0V_Pu-WAfPTkEd13MSmIns/edit#gid=747888622
+    """
 
     @pytest.mark.invalid
     @pytest.mark.SKA_mid
     def test_health_state_ok(
         self, subarray_node, simulator_factory, event_recorder
     ):
+        # Row 1
         (
             csp_sa_sim,
             sdp_sa_sim,
@@ -31,18 +35,19 @@ class TestSubarrayHealthState(object):
         event_recorder.subscribe_event(
             subarray_node.subarray_node, "healthState"
         )
-
+        # Subarray node react automatically
         assert event_recorder.has_change_event_occurred(
             subarray_node.subarray_node,
             "healthState",
             HealthState.OK,
-        )
+        ), "Expected Subarray Node HealthState to be OK"
 
     @pytest.mark.invalid
     @pytest.mark.SKA_mid
-    def test_health_state_failed(
+    def test_health_state_failed_when_csp_failed(
         self, subarray_node, simulator_factory, event_recorder
     ):
+        # Row 2
         (
             csp_sa_sim,
             sdp_sa_sim,
@@ -63,13 +68,14 @@ class TestSubarrayHealthState(object):
             subarray_node.subarray_node,
             "healthState",
             HealthState.FAILED,
-        )
+        ), "Expected Subarray Node HealthState to be FAILED"
 
     @pytest.mark.invalid
     @pytest.mark.SKA_mid
-    def test_health_state_degraded(
+    def test_health_state_degraded_when_csp_degraded(
         self, subarray_node, simulator_factory, event_recorder
     ):
+        # Row 3
         (
             csp_sa_sim,
             sdp_sa_sim,
@@ -90,13 +96,14 @@ class TestSubarrayHealthState(object):
             subarray_node.subarray_node,
             "healthState",
             HealthState.DEGRADED,
-        )
+        ), "Expected Subarray Node HealthState to be DEGRADED"
 
-    @pytest.mark.invalid
+    @pytest.mark.negative
     @pytest.mark.SKA_mid
-    def test_health_state_sdp_failed(
+    def test_health_state_failed_when_sdp_failed(
         self, subarray_node, simulator_factory, event_recorder
     ):
+        # Row 5
         (
             csp_sa_sim,
             sdp_sa_sim,
@@ -116,18 +123,19 @@ class TestSubarrayHealthState(object):
         assert event_recorder.has_change_event_occurred(
             subarray_node.subarray_node,
             "healthState",
-            HealthState.FAILED,
-        )
+            HealthState.OK,
+        ), "Expected Subarray Node HealthState to be FAILED"
 
     @pytest.mark.invalid
     @pytest.mark.SKA_mid
-    def test_health_state_dish_failed(
+    def test_health_state_failed_when_dish_failed(
         self,
         subarray_node,
         simulator_factory,
         event_recorder,
         command_input_factory,
     ):
+        # Row 9
         (
             csp_sa_sim,
             sdp_sa_sim,
@@ -135,17 +143,8 @@ class TestSubarrayHealthState(object):
             dish_master_2,
         ) = get_device_simulators(simulator_factory)
 
-        subarray_node.move_to_on()
-        subarray_node.force_change_of_obs_state("EMPTY")
-        input_json = prepare_json_args_for_commands(
-            "assign_resources_mid", command_input_factory
-        )
-
-        event_recorder.subscribe_event(subarray_node.subarray_node, "obsState")
-
-        subarray_node.execute_transition("AssignResources", argin=input_json)
-        assert event_recorder.has_change_event_occurred(
-            subarray_node.subarray_node, "obsState", ObsState.IDLE
+        self._assign_dishes_to_subarray(
+            subarray_node, command_input_factory, event_recorder
         )
 
         csp_sa_sim.SetDirectHealthState(HealthState.OK)
@@ -161,7 +160,7 @@ class TestSubarrayHealthState(object):
             subarray_node.subarray_node,
             "healthState",
             HealthState.FAILED,
-        )
+        ), "Expected Subarray Node HealthState to be FAILED"
 
     @pytest.mark.invalid
     @pytest.mark.SKA_mid
@@ -172,6 +171,7 @@ class TestSubarrayHealthState(object):
         event_recorder,
         command_input_factory,
     ):
+        # Row 8
         (
             csp_sa_sim,
             sdp_sa_sim,
@@ -179,17 +179,8 @@ class TestSubarrayHealthState(object):
             dish_master_2,
         ) = get_device_simulators(simulator_factory)
 
-        subarray_node.move_to_on()
-        subarray_node.force_change_of_obs_state("EMPTY")
-        input_json = prepare_json_args_for_commands(
-            "assign_resources_mid", command_input_factory
-        )
-
-        event_recorder.subscribe_event(subarray_node.subarray_node, "obsState")
-
-        subarray_node.execute_transition("AssignResources", argin=input_json)
-        assert event_recorder.has_change_event_occurred(
-            subarray_node.subarray_node, "obsState", ObsState.IDLE
+        self._assign_dishes_to_subarray(
+            subarray_node, command_input_factory, event_recorder
         )
 
         csp_sa_sim.SetDirectHealthState(HealthState.OK)
@@ -205,4 +196,20 @@ class TestSubarrayHealthState(object):
             subarray_node.subarray_node,
             "healthState",
             HealthState.DEGRADED,
+        ), "Expected Subarray Node HealthState to be DEGRADED"
+
+    def _assign_dishes_to_subarray(
+        self, subarray_node, command_input_factory, event_recorder
+    ):
+        subarray_node.move_to_on()
+        subarray_node.force_change_of_obs_state("EMPTY")
+        input_json = prepare_json_args_for_commands(
+            "assign_resources_mid", command_input_factory
         )
+
+        event_recorder.subscribe_event(subarray_node.subarray_node, "obsState")
+
+        subarray_node.execute_transition("AssignResources", argin=input_json)
+        assert event_recorder.has_change_event_occurred(
+            subarray_node.subarray_node, "obsState", ObsState.IDLE
+        ), "Waiting for subarray node to complete"
