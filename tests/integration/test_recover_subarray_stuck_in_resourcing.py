1df9a5edc09e552aef0527d84701282218566749
truptiska <truptin.ska@gmail.com>
2023-08-11 19:16:12 +0530

    SAH-1389 : lint issue resolved

--- tests/integration/test_recover_subarray_stuck_in_resourcing.py
+++ tests/integration/test_recover_subarray_stuck_in_resourcing.py
@@ -133,91 +133,6 @@ def test_recover_subarray_stuck_in_resourcing(
         tear_down(release_json, **ON_OFF_DEVICE_COMMAND_DICT)
 
 
-# @pytest.mark.SKA_mid
-# def test_mid_abort_restart_in_aborting_old(json_factory):
-#     """Abort and Restart is executed."""
-#     telescope_control = BaseTelescopeControl()
-#     assign_json = json_factory("command_AssignResources")
-#     release_json = json_factory("command_ReleaseResources")
-#     tmc_helper = TmcHelper(centralnode, tmc_subarraynode1)
-#
-#     try:
-#         tmc_helper.check_devices(DEVICE_LIST_FOR_CHECK_DEVICES)
-#
-#         # Verify Telescope is Off/Standby#
-#         assert telescope_control.is_in_valid_state(
-#             DEVICE_STATE_STANDBY_INFO, "State"
-#         )
-#
-#         # Invoke TelescopeOn() command on TMC#
-#         tmc_helper.set_to_on(**ON_OFF_DEVICE_COMMAND_DICT)
-#
-#         # Verify State transitions after TelescopeOn#
-#         assert telescope_control.is_in_valid_state(
-#             DEVICE_STATE_ON_INFO, "State"
-#         )
-#
-#         # Check Subarray1 availability
-#         assert check_subarray1_availability(tmc_subarraynode1)
-#
-#         csp_subarray_proxy = DeviceProxy(csp_subarray1)
-#         csp_subarray_proxy.SetDefective(True)
-#
-#         # Invoke AssignResources() Command on TMC#
-#         tmc_helper.compose_sub(assign_json, **ON_OFF_DEVICE_COMMAND_DICT)
-#
-#         # Verify ObsState is IDLE#
-#         assert telescope_control.is_in_valid_state(
-#             DEVICE_OBS_STATE_IDLE_INFO, "obsState"
-#         )
-#
-#         csp_subarray_proxy.SetDefective(True)
-#         # Invoke Abort() command on TMC#
-#         subarray_node = DeviceProxy(tmc_subarraynode1)
-#         subarray_node.Abort()
-#         resource(tmc_subarraynode1).assert_attribute("obsState").equals(
-#             "ABORTING"
-#         )
-#
-#         # Invoke Abort() command on TMC
-#         with pytest.raises(Exception):
-#             tmc_helper.invoke_abort()
-#
-#         # Verify ObsState is Aborted
-#         the_waiter = Waiter()
-#         the_waiter.set_wait_for_specific_obsstate(
-#             "ABORTED", [tmc_subarraynode1]
-#         )
-#         the_waiter.wait(200)
-#
-#         # Verify State transitions after Abort#
-#         assert telescope_control.is_in_valid_state(
-#             DEVICE_OBS_STATE_ABORT_INFO, "obsState"
-#         )
-#
-#         # Invoke Restart() command on TMC#
-#         tmc_helper.invoke_restart(**ON_OFF_DEVICE_COMMAND_DICT)
-#
-#         # Verify ObsState is EMPTY#
-#         assert telescope_control.is_in_valid_state(
-#             DEVICE_OBS_STATE_EMPTY_INFO, "obsState"
-#         )
-#
-#         # Invoke TelescopeStandby() command on TMC#
-#         tmc_helper.set_to_standby(**ON_OFF_DEVICE_COMMAND_DICT)
-#
-#         # Verify State transitions after TelescopeStandby#
-#         assert telescope_control.is_in_valid_state(
-#             DEVICE_STATE_STANDBY_INFO, "State"
-#         )
-#
-#         LOGGER.info("Test complete.")
-#
-#     except Exception:
-#         tear_down(release_json, **ON_OFF_DEVICE_COMMAND_DICT)
-
-
-@pytest.mark.trupti
 @pytest.mark.SKA_mid
 def test_recover_subarray_stuck_in_resourcing_with_abort(
     json_factory, change_event_callbacks
@@ -293,9 +208,6 @@ def test_recover_subarray_stuck_in_resourcing_with_abort(
         sdp_subarray.SetDirectObsState(0)
         assert Resource(sdp_subarray1).get("obsState") == "EMPTY"
 
-        # Invoke Abort() command on TMC
-        # with pytest.raises(Exception):
-        # tmc_helper.invoke_abort()
         subarray_node = DeviceProxy(tmc_subarraynode1)
         subarray_node.Abort()
 
