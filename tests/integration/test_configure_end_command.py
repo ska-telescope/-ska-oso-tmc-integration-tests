a1f5b9deb4b494cd76b1d7884e07359dc8970dba
shraddhabajare <shraddhab.ska@gmail.com>
2023-09-11 16:10:15 +0530

    SAH-1391: Minor updates

--- tests/integration/test_configure_end_command.py
+++ tests/integration/test_configure_end_command.py
@@ -40,64 +40,6 @@ telescope_control = BaseTelescopeControl()
 
 
 @pytest.mark.SKA_mid
-def test_configure_end(json_factory):
-    """Configure and End is executed."""
-    release_json = json_factory("command_ReleaseResources")
-    assign_json = json_factory("command_AssignResources")
-    configure_json = json_factory("command_Configure")
-    try:
-        # Verify Telescope is Off/Standby
-        tmc_helper.check_devices(DEVICE_LIST_FOR_CHECK_DEVICES)
-        assert telescope_control.is_in_valid_state(
-            DEVICE_STATE_STANDBY_INFO, "State"
-        )
-        # Invoke TelescopeOn() command on TMC CentralNode
-        tmc_helper.set_to_on(**ON_OFF_DEVICE_COMMAND_DICT)
-        # Verify State transitions after TelescopeOn
-        assert telescope_control.is_in_valid_state(
-            DEVICE_STATE_ON_INFO, "State"
-        )
-
-        # Check Subarray1 availability
-        assert check_subarray1_availability(tmc_subarraynode1)
-
-        # Invoke AssignResources() Command on TMC
-        tmc_helper.compose_sub(assign_json, **ON_OFF_DEVICE_COMMAND_DICT)
-        assert telescope_control.is_in_valid_state(
-            DEVICE_OBS_STATE_IDLE_INFO, "obsState"
-        )
-
-        # Invoke Configure() Command on TMC
-        tmc_helper.configure_subarray(
-            configure_json, **ON_OFF_DEVICE_COMMAND_DICT
-        )
-        assert telescope_control.is_in_valid_state(
-            DEVICE_OBS_STATE_READY_INFO, "obsState"
-        )
-        # Teardowning
-        # Invoke End() Command on TMC
-        tmc_helper.end(**ON_OFF_DEVICE_COMMAND_DICT)
-        assert telescope_control.is_in_valid_state(
-            DEVICE_OBS_STATE_IDLE_INFO, "obsState"
-        )
-        # Invoke ReleaseResources() command on TMC
-        tmc_helper.invoke_releaseResources(
-            release_json, **ON_OFF_DEVICE_COMMAND_DICT
-        )
-        assert telescope_control.is_in_valid_state(
-            DEVICE_OBS_STATE_EMPTY_INFO, "obsState"
-        )
-        # Invoke Standby() command on TMC
-        tmc_helper.set_to_standby(**ON_OFF_DEVICE_COMMAND_DICT)
-        assert telescope_control.is_in_valid_state(
-            DEVICE_STATE_STANDBY_INFO, "State"
-        )
-    except Exception as e:
-        LOGGER.exception("The exception is: %s", e)
-        tear_down(release_json, **ON_OFF_DEVICE_COMMAND_DICT)
-
-
-@pytest.mark.SKA_mid
 def test_configure_timeout_and_error_propagation_csp(
     json_factory, change_event_callbacks
 ):
