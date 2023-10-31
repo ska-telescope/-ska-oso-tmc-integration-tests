1288e1b6e0e4a7ffeb2c9f3f92e7400f23aa1f88
Aditya Dange <adityadange.ska@gmail.com>
2023-10-31 12:04:21 +0530

    HM-335 Fix formatting

--- tests/integration/tmc_robustness/test_successful_scan_after_failed_configure.py
+++ tests/integration/tmc_robustness/test_successful_scan_after_failed_configure.py
@@ -39,7 +39,8 @@ telescope_control = BaseTelescopeControl()
 # )
 @scenario(
     "../features/xtp-20321.feature",
-    "Successfully execute a scan after a failed attempt to configure the subarray, use already assigned resources"
+    "Successfully execute a scan after a failed attempt to configure the\
+          subarray, use already assigned resources",
 )
 def test_configure_resource_with_invalid_json():
     """
