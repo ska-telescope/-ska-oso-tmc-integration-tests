56d8b6ed4809e98593da69c4d6e795cdff5bd9f9
akshaysonawane <akshayss.ska@gmail.com>
2023-11-11 12:24:27 +0530

    HM-354: update the test case

--- tests/integration/test_subsystems/test_telescope_realdish_configure_command.py
+++ tests/integration/test_subsystems/test_telescope_realdish_configure_command.py
@@ -8,7 +8,8 @@ from tests.conftest import wait_for_dish_mode_change
 from tests.resources.test_support.common_utils.common_helpers import Waiter
 from tests.resources.test_support.constant import (
     centralnode,
-    dish_fqdn,
+    dish_fqdn_1,
+    dish_fqdn_2,
     tmc_subarraynode1,
 )
 from tests.resources.test_support.enum import DishMode
@@ -28,28 +29,35 @@ def test_configure(json_factory):
     central_node_device.TelescopeOn()
 
     # Check the dishMode and dishleafnode state
-    dishfqdn = DeviceProxy(dish_fqdn)
+    dishfqdn1 = DeviceProxy(dish_fqdn_1)
+    dishfqdn2 = DeviceProxy(dish_fqdn_2)
 
     # Waiting for DISH LMC to respond
-    wait_for_dish_mode_change(DishMode.STANDBY_FP, dishfqdn, 30)
+    wait_for_dish_mode_change(DishMode.STANDBY_FP, dishfqdn1, 30)
+    wait_for_dish_mode_change(DishMode.STANDBY_FP, dishfqdn2, 30)
 
     # Check the dishMode of DISH LMC i.e STANDBYFP
-    assert dishfqdn.dishMode.value == DishMode.STANDBY_FP
+    assert dishfqdn1.dishMode.value == DishMode.STANDBY_FP
+    assert dishfqdn2.dishMode.value == DishMode.STANDBY_FP
 
     # invoke assignresources command from central node
     central_node_device.AssignResources(assign_json)
-    time.sleep(5)
+    time.sleep(10)
     # invoke configure command from subarray node
     subarray.Configure(config_json)
 
-    wait_for_dish_mode_change(DishMode.OPERATE, dishfqdn, 30)
+    wait_for_dish_mode_change(DishMode.OPERATE, dishfqdn1, 30)
+    wait_for_dish_mode_change(DishMode.OPERATE, dishfqdn2, 30)
 
     the_waiter.set_wait_for_specific_obsstate("READY", [subarray])
     the_waiter.wait(400)
     # invoke end command from subarray node
     subarray.End()
 
-    the_waiter.set_wait_for_pointingstate("READY", [dishfqdn])
+    the_waiter.set_wait_for_pointingstate("READY", [dishfqdn1])
+    the_waiter.wait(400)
+
+    the_waiter.set_wait_for_pointingstate("READY", [dishfqdn2])
     the_waiter.wait(400)
 
     the_waiter.set_wait_for_specific_obsstate("IDLE", [subarray])
@@ -60,7 +68,9 @@ def test_configure(json_factory):
     central_node_device.TelescopeOff()
 
     # Waiting for DISH LMC to respond
-    wait_for_dish_mode_change(DishMode.STANDBY_LP, dishfqdn, 30)
+    wait_for_dish_mode_change(DishMode.STANDBY_LP, dishfqdn1, 30)
+    wait_for_dish_mode_change(DishMode.STANDBY_LP, dishfqdn2, 30)
 
     # check the dishMode of DISH LMC i.e STANDBYLP
-    assert dishfqdn.dishMode.value == DishMode.STANDBY_LP
+    assert dishfqdn1.dishMode.value == DishMode.STANDBY_LP
+    assert dishfqdn2.dishMode.value == DishMode.STANDBY_LP
