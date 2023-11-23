d554e7089cb0edfcc2b628def8206c08d4fe323d
Muskan Sharma <muskans.ska@gmail.com>
2023-11-23 17:24:46 +0530

    HM-352: Modified test cases.

--- tests/integration/tmc_harness/bdd_scenarios/test_tmc_scan_command.py
+++ tests/integration/tmc_harness/bdd_scenarios/test_tmc_scan_command.py
@@ -3,12 +3,12 @@ from pytest_bdd import given, scenario, then, when
 from ska_control_model import ObsState
 from tango import DevState
 
+from tests.resources.test_harness.constant import tmc_low_subarraynode1
 from tests.resources.test_harness.helpers import (
-    check_subarray_obs_state,
     prepare_json_args_for_centralnode_commands,
     prepare_json_args_for_commands,
 )
-from tests.resources.test_harness.utils.enums import SimulatorDeviceType
+from tests.resources.test_support.common_utils.common_helpers import Waiter
 
 
 @pytest.mark.SKA_low
@@ -47,41 +47,13 @@ def given_subarray_in_ready(
     central_node_low,
     subarray_node_low,
     event_recorder,
-    simulator_factory,
 ):
     """Set up a subarray in the READY obsState."""
-    csp_subarray_sim = simulator_factory.get_or_create_simulator_device(
-        SimulatorDeviceType.LOW_CSP_DEVICE
-    )
-    sdp_subarray_sim = simulator_factory.get_or_create_simulator_device(
-        SimulatorDeviceType.LOW_SDP_DEVICE
-    )
-    mccs_subarray_sim = simulator_factory.get_or_create_simulator_device(
-        SimulatorDeviceType.MCCS_SUBARRAY_DEVICE
-    )
-    event_recorder.subscribe_event(csp_subarray_sim, "obsState")
-    event_recorder.subscribe_event(sdp_subarray_sim, "obsState")
-    event_recorder.subscribe_event(mccs_subarray_sim, "obsState")
     event_recorder.subscribe_event(central_node_low.subarray_node, "obsState")
     assign_input_json = prepare_json_args_for_centralnode_commands(
         "assign_resources_low", command_input_factory
     )
     central_node_low.perform_action("AssignResources", assign_input_json)
-    assert event_recorder.has_change_event_occurred(
-        csp_subarray_sim,
-        "obsState",
-        ObsState.IDLE,
-    )
-    assert event_recorder.has_change_event_occurred(
-        sdp_subarray_sim,
-        "obsState",
-        ObsState.IDLE,
-    )
-    assert event_recorder.has_change_event_occurred(
-        mccs_subarray_sim,
-        "obsState",
-        ObsState.IDLE,
-    )
     assert event_recorder.has_change_event_occurred(
         central_node_low.subarray_node,
         "obsState",
@@ -91,21 +63,6 @@ def given_subarray_in_ready(
         "configure_low", command_input_factory
     )
     subarray_node_low.execute_transition("Configure", configure_input_json)
-    assert event_recorder.has_change_event_occurred(
-        csp_subarray_sim,
-        "obsState",
-        ObsState.READY,
-    )
-    assert event_recorder.has_change_event_occurred(
-        sdp_subarray_sim,
-        "obsState",
-        ObsState.READY,
-    )
-    assert event_recorder.has_change_event_occurred(
-        mccs_subarray_sim,
-        "obsState",
-        ObsState.READY,
-    )
     assert event_recorder.has_change_event_occurred(
         central_node_low.subarray_node,
         "obsState",
@@ -129,36 +86,13 @@ def send_scan(
 def check_scan_completion(
     subarray_node_low,
     event_recorder,
-    simulator_factory,
 ):
     """Verify that the subarray is in the SCANNING obsState."""
-    csp_subarray_sim = simulator_factory.get_or_create_simulator_device(
-        SimulatorDeviceType.LOW_CSP_DEVICE
-    )
-    sdp_subarray_sim = simulator_factory.get_or_create_simulator_device(
-        SimulatorDeviceType.LOW_SDP_DEVICE
-    )
-    mccs_subarray_sim = simulator_factory.get_or_create_simulator_device(
-        SimulatorDeviceType.MCCS_SUBARRAY_DEVICE
-    )
-    assert event_recorder.has_change_event_occurred(
-        csp_subarray_sim,
-        "obsState",
-        ObsState.SCANNING,
-    )
-    assert event_recorder.has_change_event_occurred(
-        sdp_subarray_sim,
-        "obsState",
-        ObsState.SCANNING,
-    )
-    assert event_recorder.has_change_event_occurred(
-        mccs_subarray_sim,
-        "obsState",
-        ObsState.SCANNING,
-    )
     assert event_recorder.has_change_event_occurred(
         subarray_node_low.subarray_node,
         "obsState",
         ObsState.SCANNING,
     )
-    assert check_subarray_obs_state("READY")
+    the_waiter = Waiter()
+    the_waiter.set_wait_for_specific_obsstate("READY", [tmc_low_subarraynode1])
+    the_waiter.wait(200)
--- tests/integration/tmc_harness/test_abort_command_not_allowed_empty.py
+++ tests/integration/tmc_harness/test_abort_command_not_allowed_empty.py
@@ -1,10 +1,17 @@
 import pytest
+import tango
 from pytest_bdd import given, scenario, then, when
 from ska_control_model import ObsState
 
-from tests.resources.test_support.common_utils.result_code import ResultCode
+from tests.resources.test_support.common_utils.telescope_controls import (
+    BaseTelescopeControl,
+)
+from tests.resources.test_support.constant_low import (
+    DEVICE_OBS_STATE_EMPTY_INFO,
+)
 
 result, message = "", ""
+telescope_control = BaseTelescopeControl()
 
 
 @pytest.mark.SKA_low
@@ -34,23 +41,22 @@ def invoke_abort_command(
     subarray_node_low,
 ):
     """Send a Abort command to the subarray."""
-    result_code, unique_id = subarray_node_low.execute_transition("Abort")
-    assert result_code[0] == ResultCode.STARTED
+    try:
+        subarray_node_low.execute_transition("Abort")
+    except tango.DevFailed as ex:
+        pytest.command_result = str(ex)
 
 
-@then("TMC should reject the Abort command with ResultCode.Rejected")
+@then("TMC should reject the command with ResultCode.REJECTED")
 def invalid_command_rejection():
     assert (
-        "Abort command is not allowed in current subarray obsState"
-        in message[0]
+        "Abort command not permitted in observation state EMPTY"
+        in pytest.command_result
     )
-    assert result[0] == ResultCode.REJECTED
 
 
 @then("the Subarray remains in obsState EMPTY")
-def tmc_status(subarray_node_low, event_recorder):
-    assert event_recorder.has_change_event_occurred(
-        subarray_node_low.subarray_node,
-        "obsState",
-        ObsState.EMPTY,
+def tmc_status():
+    assert telescope_control.is_in_valid_state(
+        DEVICE_OBS_STATE_EMPTY_INFO, "obsState"
     )
