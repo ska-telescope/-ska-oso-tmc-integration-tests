0b637bbee76433cd42b1b9a81bfd9e89edce7657
apurvapatkar <apurva.ska@gmail.com>
2023-12-14 17:19:50 +0530

    SAH-1446: Check 4th dish instance.

--- tests/integration/test_subsystems/test_telescope_on_command_real_dish.py
+++ tests/integration/test_subsystems/test_telescope_on_command_real_dish.py
@@ -7,11 +7,12 @@ from tango import DeviceProxy
 
 from tests.conftest import wait_for_dish_mode_change
 from tests.resources.test_support.common_utils.common_helpers import Waiter
-from tests.resources.test_support.constant import (  # dish_fqdn_100,
+from tests.resources.test_support.constant import (
     centralnode,
     dish_fqdn_1,
     dish_fqdn_36,
     dish_fqdn_63,
+    dish_fqdn_100,
 )
 from tests.resources.test_support.enum import DishMode
 
@@ -34,13 +35,13 @@ def test_telescope_on():
     LOGGER.info("Dish 36: %s", dish_master_36.dev_name())
     dish_master_63 = DeviceProxy(dish_fqdn_63)
     LOGGER.info("Dish 63: %s", dish_master_63.dev_name())
-    # dish_master_100 = DeviceProxy(dish_fqdn_100)
+    dish_master_100 = DeviceProxy(dish_fqdn_100)
 
     # Waiting for DISH LMC to respond
     wait_for_dish_mode_change(DishMode.STANDBY_FP, dish_master_1, 30)
     wait_for_dish_mode_change(DishMode.STANDBY_FP, dish_master_36, 30)
     wait_for_dish_mode_change(DishMode.STANDBY_FP, dish_master_63, 30)
-    # wait_for_dish_mode_change(DishMode.STANDBY_FP, dish_master_100, 30)
+    wait_for_dish_mode_change(DishMode.STANDBY_FP, dish_master_100, 30)
 
     the_waiter.wait(50)
 
@@ -48,7 +49,7 @@ def test_telescope_on():
     assert dish_master_1.dishMode.value == DishMode.STANDBY_FP
     assert dish_master_36.dishMode.value == DishMode.STANDBY_FP
     assert dish_master_63.dishMode.value == DishMode.STANDBY_FP
-    # assert dish_master_100.dishMode.value == DishMode.STANDBY_FP
+    assert dish_master_100.dishMode.value == DishMode.STANDBY_FP
 
     # Invoke TelescopeOff command
 
@@ -58,10 +59,10 @@ def test_telescope_on():
     wait_for_dish_mode_change(DishMode.STANDBY_LP, dish_master_1, 30)
     wait_for_dish_mode_change(DishMode.STANDBY_LP, dish_master_36, 30)
     wait_for_dish_mode_change(DishMode.STANDBY_LP, dish_master_63, 30)
-    # wait_for_dish_mode_change(DishMode.STANDBY_LP, dish_master_100, 30)
+    wait_for_dish_mode_change(DishMode.STANDBY_LP, dish_master_100, 30)
 
     # check the dishMode of DISH LMC i.e STANDBYLP
     assert dish_master_1.dishMode.value == DishMode.STANDBY_LP
     assert dish_master_36.dishMode.value == DishMode.STANDBY_LP
     assert dish_master_63.dishMode.value == DishMode.STANDBY_LP
-    # assert dish_master_100.dishMode.value == DishMode.STANDBY_LP
+    assert dish_master_100.dishMode.value == DishMode.STANDBY_LP
