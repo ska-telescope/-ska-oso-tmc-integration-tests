99cc4514072accc9d57a33fd0266ba4824ee317a
shraddhabajare <shraddhab.ska@gmail.com>
2023-12-21 11:33:13 +0530

    SAH-1449: Modify the scenario according to the agreed confluence

--- tests/integration/test_tmc_sdp_on.py
+++ tests/integration/test_tmc_sdp_on.py
@@ -6,7 +6,7 @@ from tango import DevState
 from tests.resources.test_harness.helpers import get_master_device_simulators
 
 
-@pytest.mark.real_sdp1
+@pytest.mark.real_sdp
 @scenario(
     "../features/start_up_tmc_sdp.feature",
     "Start up the telescope having TMC and SDP subsystems",
@@ -23,14 +23,10 @@ def test_tmc_sdp_startup_telescope():
     """
 
 
-@given("a Telescope consisting of TMC, SDP, simulated CSP and simulated DISH")
+@given("a Telescope consisting of TMC, SDP, simulated CSP and simulated Dish")
 def given_a_tmc(central_node_mid, simulator_factory):
     """
     Given a TMC
-
-    Args:
-        simulator_factory: fixture for SimulatorFactory class,
-        which provides simulated subarray and master devices
     """
     (
         csp_master_sim,
@@ -45,6 +41,19 @@ def given_a_tmc(central_node_mid, simulator_factory):
     assert csp_master_sim.ping() > 0
     assert dish_master_sim_1.ping() > 0
     assert dish_master_sim_2.ping() > 0
+
+
+@given("telescope state is STANDBY")
+def check_telescope_state_standby(central_node_mid, event_recorder):
+    """A method to check CentralNode telescopeState STANDBY"""
+    event_recorder.subscribe_event(
+        central_node_mid.central_node, "telescopeState"
+    )
+    assert event_recorder.has_change_event_occurred(
+        central_node_mid.central_node,
+        "telescopeState",
+        DevState.STANDBY,
+    )
 
 
 @when("I start up the telescope")
@@ -75,9 +84,6 @@ def check_sdp_is_on(central_node_mid, event_recorder):
 @then("telescope state is ON")
 def check_telescope_state(central_node_mid, event_recorder):
     """A method to check CentralNode.telescopeState"""
-    event_recorder.subscribe_event(
-        central_node_mid.central_node, "telescopeState"
-    )
     assert event_recorder.has_change_event_occurred(
         central_node_mid.central_node,
         "telescopeState",
