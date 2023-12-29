9c91e7662aea619e1d57774c1f183ab22faea67a
apurvapatkar <apurva.ska@gmail.com>
2023-12-29 17:49:02 +0530

    SAH-1446: Execute tests.

--- tests/resources/test_support/common_utils/sync_decorators.py
+++ tests/resources/test_support/common_utils/sync_decorators.py
@@ -179,14 +179,14 @@ def sync_configure():
             the_waiter = Waiter(**kwargs)
             if Resource(kwargs.get("tmc_subarraynode")) == "READY":
                 invoked_from_ready = True
-            LOGGER.info("invoked_from_ready flag: %s", invoked_from_ready)
+            LOGGER.debug("invoked_from_ready flag: %s", invoked_from_ready)
             result = func(*args, **kwargs)
             set_wait_for_obsstate = kwargs.get("set_wait_for_obsstate", True)
-            LOGGER.info("set_wait_for_obsstate: %s", set_wait_for_obsstate)
+            LOGGER.debug("set_wait_for_obsstate: %s", set_wait_for_obsstate)
             if set_wait_for_obsstate:
-                # if invoked_from_ready:
-                the_waiter.set_wait_for_configuring()
-                the_waiter.wait(500)
+                if invoked_from_ready:
+                    the_waiter.set_wait_for_configuring()
+                    the_waiter.wait(500)
                 the_waiter.set_wait_for_configure()
                 the_waiter.wait(800)
             return result
