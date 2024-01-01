1b7b8c78feeae000b401e69bade0a81a667af583
manthan <manthan.ska@gmail.com>
2024-01-01 18:34:22 +0530

    SAH-1464: Update scan test case

--- tests/tmc_csp/test_tmc_scan.py
+++ tests/tmc_csp/test_tmc_scan.py
@@ -11,7 +11,6 @@ from tests.resources.test_harness.event_recorder import EventRecorder
 from tests.resources.test_harness.helpers import (
     prepare_json_args_for_centralnode_commands,
     prepare_json_args_for_commands,
-    wait_csp_master_off,
 )
 from tests.resources.test_harness.utils.common_utils import JsonFactory
 
@@ -34,13 +33,13 @@ def given_a_telescope_in_on_state(central_node_mid, event_recorder):
     event_recorder.subscribe_event(
         central_node_mid.central_node, "telescopeState"
     )
-    central_node_mid.csp_master.adminMode = 0
-    wait_csp_master_off()
-    central_node_mid.move_to_on()
     event_recorder.subscribe_event(central_node_mid.csp_master, "State")
     event_recorder.subscribe_event(
         central_node_mid.subarray_devices["csp_subarray"], "State"
     )
+    if central_node_mid.telescope_state != "ON":
+        central_node_mid.move_to_on()
+
     assert event_recorder.has_change_event_occurred(
         central_node_mid.csp_master,
         "State",
@@ -110,12 +109,14 @@ def invoke_scan(central_node_mid, command_input_factory):
 @then(parsers.parse("the CSP subarray transitions to ObsState SCANNING"))
 def csp_subarray_scanning(central_node_mid, event_recorder, subarray_id):
     """Checks if Csp Subarray's obsState attribute value is SCANNING"""
-    central_node_mid.set_subarray_id(int(subarray_id))
+    event_recorder.subscribe_event(
+        central_node_mid.subarray_devices["csp_subarray"], "obsState"
+    )
     assert event_recorder.has_change_event_occurred(
-        central_node_mid.subarray_node,
+        central_node_mid.subarray_devices["csp_subarray"],
         "obsState",
         ObsState.SCANNING,
-        lookahead=15,
+        lookahead=20,
     )
 
 
@@ -132,15 +133,6 @@ def tmc_subarray_scanning(central_node_mid, event_recorder, subarray_id):
         "obsState",
         ObsState.SCANNING,
         lookahead=15,
-    )
-    event_recorder.subscribe_event(
-        central_node_mid.subarray_devices["csp_subarray"], "obsState"
-    )
-    assert event_recorder.has_change_event_occurred(
-        central_node_mid.subarray_devices["csp_subarray"],
-        "obsState",
-        ObsState.SCANNING,
-        lookahead=20,
     )
 
 
