3f98adac117db23efdf88efe04b286f53c1d9e53
apurvapatkar <apurva.ska@gmail.com>
2024-01-01 16:19:23 +0530

    SAH-1456: Execute Dish Configure test.

--- tests/conftest.py
+++ tests/conftest.py
@@ -181,10 +181,8 @@ def wait_for_telescope_state_change(
     start_time = time.time()
     LOGGER.info("target_state: %s", target_state)
     while time.time() - start_time < timeout_seconds:
-        LOGGER.info(
-            "CN.telescopeState: %s", centralnode_fqdn.telescopeState.value
-        )
-        if centralnode_fqdn.telescopeState.value == target_state:
+        LOGGER.info("CN.telescopeState: %s", centralnode_fqdn.telescopeState)
+        if centralnode_fqdn.telescopeState == target_state:
             return True
         time.sleep(1)
 
