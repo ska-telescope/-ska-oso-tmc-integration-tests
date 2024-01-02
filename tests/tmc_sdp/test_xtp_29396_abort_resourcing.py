c98ae67d6373a92007ce95a8e176db38d508ba38
Mansi Shirore <mansis.ska@gmail.com>
2024-01-02 19:56:00 +0530

    SAH-1466: Updated test case.

--- tests/tmc_sdp/test_xtp_29396_abort_resourcing.py
+++ tests/tmc_sdp/test_xtp_29396_abort_resourcing.py
@@ -14,7 +14,7 @@ from tests.resources.test_harness.helpers import (
     "../features/tmc_sdp/xtp_29396_abort_resourcing.feature",
     "Abort assigning using TMC",
 )
-def test_tmc_sdp_abort_in_resourcing(central_node_mid):
+def test_tmc_sdp_abort_in_resourcing():
     """
     Test case to verify TMC-SDP Abort functionality in RESOURCING obsState
     """
@@ -54,11 +54,6 @@ def telescope_is_in_resourcing_obsstate(
     )
 
 
-@when(
-    parsers.parse(
-        "I issued the Abort command to the TMC subarray {subarray_id}"
-    )
-)
 @when(parsers.parse("I command it to Abort"))
 def abort_is_invoked(central_node_mid):
     """
@@ -79,11 +74,6 @@ def sdp_subarray_is_in_aborted_obsstate(central_node_mid, event_recorder):
     )
 
 
-@then(
-    parsers.parse(
-        "the TMC subarray {subarray_id} transitions to ABORTED ObsState"
-    )
-)
 @then(parsers.parse("the TMC subarray obsState transitions to ABORTED"))
 def tmc_subarray_is_in_aborted_obsstate(central_node_mid, event_recorder):
     """
@@ -93,57 +83,4 @@ def tmc_subarray_is_in_aborted_obsstate(central_node_mid, event_recorder):
         central_node_mid.subarray_node,
         "obsState",
         ObsState.ABORTED,
-    )
-
-
-@pytest.mark.tmc_sdp
-@scenario(
-    "../features/tmc_sdp/xtp_29397_abort_idle.feature",
-    "TMC executes an Abort on SDP subarray while subarray completes"
-    + " configuration",
-)
-def test_tmc_sdp_abort_in_idle(central_node_mid):
-    """
-    Test case to verify TMC-SDP Abort functionality in IDLE obsState
-    """
-
-
-@given("the telescope is in ON state")
-def telescope_is_in_on_state(central_node_mid, event_recorder):
-    """
-    This method checks if the telescope is in ON state
-    """
-    central_node_mid.move_to_on()
-    event_recorder.subscribe_event(
-        central_node_mid.central_node, "telescopeState"
-    )
-    assert event_recorder.has_change_event_occurred(
-        central_node_mid.central_node,
-        "telescopeState",
-        DevState.ON,
-    )
-
-
-@given(parsers.parse("TMC and SDP subarray is in {obsstate} ObsState"))
-def telescope_is_in_idle_obsstate(
-    central_node_mid, event_recorder, command_input_factory, obsstate
-):
-    """ "A method to check if telescope in is IDLE obsSstate."""
-    assign_input_json = prepare_json_args_for_centralnode_commands(
-        "assign_resources_mid", command_input_factory
-    )
-    central_node_mid.store_resources(assign_input_json)
-    event_recorder.subscribe_event(
-        central_node_mid.subarray_devices.get("sdp_subarray"), "obsState"
-    )
-    event_recorder.subscribe_event(central_node_mid.subarray_node, "obsState")
-    assert event_recorder.has_change_event_occurred(
-        central_node_mid.subarray_devices.get("sdp_subarray"),
-        "obsState",
-        ObsState[obsstate],
-    )
-    assert event_recorder.has_change_event_occurred(
-        central_node_mid.subarray_node,
-        "obsState",
-        ObsState[obsstate],
     )
