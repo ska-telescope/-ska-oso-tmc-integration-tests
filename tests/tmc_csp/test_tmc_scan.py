b81d20b682c89f8a82f5611f115f32c7271d65b8
manthan <manthan.ska@gmail.com>
2024-01-02 22:36:18 +0530

    SAH-1464: Update on scenario

--- tests/tmc_csp/test_tmc_scan.py
+++ tests/tmc_csp/test_tmc_scan.py
@@ -11,6 +11,7 @@ from tests.resources.test_harness.event_recorder import EventRecorder
 from tests.resources.test_harness.helpers import (
     prepare_json_args_for_centralnode_commands,
     prepare_json_args_for_commands,
+    wait_csp_master_off,
 )
 from tests.resources.test_harness.utils.common_utils import JsonFactory
 
@@ -33,12 +34,14 @@ def given_a_telescope_in_on_state(central_node_mid, event_recorder):
     event_recorder.subscribe_event(
         central_node_mid.central_node, "telescopeState"
     )
+    central_node_mid.csp_master.adminMode = 0
+    wait_csp_master_off()
+    central_node_mid.move_to_on()
+
     event_recorder.subscribe_event(central_node_mid.csp_master, "State")
     event_recorder.subscribe_event(
         central_node_mid.subarray_devices["csp_subarray"], "State"
     )
-    if central_node_mid.telescope_state != "ON":
-        central_node_mid.move_to_on()
 
     assert event_recorder.has_change_event_occurred(
         central_node_mid.csp_master,
