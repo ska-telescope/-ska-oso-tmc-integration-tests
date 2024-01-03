a3219be6dd85d7bead8f5940f5dc58d29ca32b97
manthan <manthan.ska@gmail.com>
2024-01-03 17:41:10 +0530

    SAH-1464: Remove marker

--- tests/tmc_csp/test_tmc_csp_endscan.py
+++ tests/tmc_csp/test_tmc_csp_endscan.py
@@ -14,8 +14,7 @@ from tests.resources.test_harness.helpers import (
 LOGGER = logging.getLogger(__name__)
 
 
-@pytest.mark.skip
-@pytest.mark.real_csp_mid
+@pytest.mark.real_csp_mid_MA
 @scenario(
     "../features/tmc_csp/tmc_csp_endscan.feature",
     "TMC executes a EndScan command on CSP subarray.",
