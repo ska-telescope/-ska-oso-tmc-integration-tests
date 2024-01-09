255e77bd7b2da2261545aadc59b131a73e31639d
manthan <manthan.ska@gmail.com>
2024-01-09 18:26:37 +0530

    SAH-1464: Update sysnc decorators

--- tests/resources/test_harness/utils/sync_decorators.py
+++ tests/resources/test_harness/utils/sync_decorators.py
@@ -167,7 +167,7 @@ def sync_end(device_dict):
     return decorator_sync_end
 
 
-def sync_endscan():
+def sync_endscan(device_dict):
     """Sync method for endscan command"""
 
     def decorator_sync_end_scan(func):
@@ -176,7 +176,7 @@ def sync_endscan():
         @functools.wraps(func)
         def wrapper(*args, **kwargs):
             """Wrapper"""
-            the_waiter = Waiter(**kwargs)
+            the_waiter = Waiter(device_dict)
             the_waiter.set_wait_for_ready()
             result = func(*args, **kwargs)
             the_waiter.wait(200)
