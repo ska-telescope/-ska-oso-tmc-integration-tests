189dde0d246f2fdc819aff0e21658ee627ac5831
manthan <manthan.ska@gmail.com>
2024-01-09 18:00:08 +0530

    SAH-1464: Resolved review comment

--- tests/resources/test_harness/subarray_node.py
+++ tests/resources/test_harness/subarray_node.py
@@ -44,6 +44,7 @@ from tests.resources.test_harness.utils.sync_decorators import (
     sync_assign_resources,
     sync_configure,
     sync_end,
+    sync_endscan,
     sync_release_resources,
     sync_restart,
 )
@@ -191,6 +192,7 @@ class SubarrayNodeWrapper(object):
         LOGGER.info("Invoked End on SubarrayNode")
         return result, message
 
+    @sync_endscan(device_dict=device_dict)
     def remove_scan_data(self):
         result, message = self.subarray_node.EndScan()
         LOGGER.info("Invoked EndScan on SubarrayNode")
