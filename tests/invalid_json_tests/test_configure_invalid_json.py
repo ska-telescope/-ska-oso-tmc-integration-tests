73ffdf1ab76620751749072bb66e507e543dce2f
Mansi Shirore <mansis.ska@gmail.com>
2024-01-19 15:48:10 +0530

    SAH-1471: Utilised centralnode dev image

--- tests/invalid_json_tests/test_configure_invalid_json.py
+++ tests/invalid_json_tests/test_configure_invalid_json.py
@@ -29,7 +29,6 @@ tmc_helper = TmcHelper(centralnode, tmc_subarraynode1)
 telescope_control = BaseTelescopeControl()
 
 
-@pytest.mark.ms1
 @pytest.mark.SKA_mid
 @scenario(
     "../features/check_invalid_json_not_allowed.feature",
