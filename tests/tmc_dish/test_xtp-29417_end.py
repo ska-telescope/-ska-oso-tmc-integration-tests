268fca27ae136bc861232e99878df56e4432f216
devesh.ska <devesh.ska@gmail.com>
2024-01-31 14:11:56 +0530

    SKB-270:fix lint issues

--- tests/tmc_dish/test_xtp-29417_end.py
+++ tests/tmc_dish/test_xtp-29417_end.py
@@ -14,6 +14,7 @@ from tests.resources.test_harness.helpers import (
 from tests.resources.test_harness.utils.enums import SimulatorDeviceType
 from tests.resources.test_support.enum import DishMode, PointingState
 
+
 @pytest.mark.skip(reason="This test will be fixed as part of sah-1476")
 @pytest.mark.tmc_dish
 @scenario(
