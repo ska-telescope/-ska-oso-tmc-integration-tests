0ff5d9d4d2d52ed9cfc0a0389736d3c206e68c5e
Roshan Tayde <roshant.ska@gmail.com>
2024-02-22 20:41:37 +0530

    HM-422: Resolve review comment

--- tests/tmc_dish/test_xtp-30385_scan.py
+++ tests/tmc_dish/test_xtp-30385_scan.py
@@ -19,7 +19,7 @@ from tests.resources.test_harness.utils.enums import SimulatorDeviceType
 from tests.resources.test_support.enum import DishMode, PointingState
 
 
-@pytest.mark.tmc_dish
+@pytest.mark.tmc_dish_scan
 @scenario(
     "../features/tmc_dish/xtp-30385_scan.feature",
     "TMC executes Scan command on DISH.LMC",
